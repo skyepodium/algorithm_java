e9009c7b58af6719d6b6731cefbfd14920a71ba0
김정윤 <skyepodium@gmail.com>
2018-12-23 19:46:31 +0900

    baekjoon 1463 by DP

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,19 @@ import java.util.*;
 
 public class Main {
 
-    static String a;
+    static int n;
+    static int[] d = new int[1000001];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        a = sc.next();
-        System.out.println(a.length());
+        n = sc.nextInt();
+
+        for(int i=2; i<=n; i++){
+            d[i] = d[i-1] + 1;
+            if(i%2 == 0) d[i] = Math.min(d[i], d[i/2] + 1);
+            if(i%3 == 0) d[i] = Math.min(d[i], d[i/3] + 1);
+        }
+        System.out.println(d[n]);
     }
 
 
