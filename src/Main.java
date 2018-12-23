9d8878cfa1e0f3a8e3118d6aac92ca25871aa3bd
김정윤 <skyepodium@gmail.com>
2018-12-23 18:57:18 +0900

    baekjoon 2743

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,13 @@ import java.util.*;
 
 public class Main {
 
-    static String a, b, c, d;
-    static long first, second;
+    static String a;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         a = sc.next();
-        b = sc.next();
-        c = sc.next();
-        d = sc.next();
-
-        a = a + b;
-        c = c + d;
-        first = sToInt(a);
-        second = sToInt(c);
-        System.out.println(first + second);
+        System.out.println(a.length());
     }
 
-    public static long sToInt(String s){
-        long result = 0;
-        for(int i=s.length() - 1; i>=0; i--){
-            result += (s.charAt(i) - '0') * Math.pow(10, s.length() -1 - i);
-        }
-        return result;
-    }
 
 }
