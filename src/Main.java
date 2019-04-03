6358fee037dbeccbd17ce0e4868b456a411a76df
김정윤 <skyepodium@gmail.com>
2019-04-03 23:35:13 +0900

    04.03

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,7 @@
 import java.util.*;
 
-
-public class Main {
-
-    static int t, n;
+public class Main{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        t = sc.nextInt();
-
-        for(int test_case = 1; test_case <= t; test_case++){
-            float total = 0;
-            float a;
-            int b;
-            n = sc.nextInt();
-            for(int i=0; i<n; i++){
-                a = sc.nextFloat();
-                b = sc.nextInt();
-                total += (a*b);
-            }
-            System.out.println("#" + test_case + " " + total);
-        }
 
     }
-
-
 }
