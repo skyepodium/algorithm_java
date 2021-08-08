099037e47976842c2bcd05a92499cff7ed7d017f
skyepodium <skyepodium@gmail.com>
2021-08-08 14:24:40 +0900

    leet code logest palindrome substring

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,54 @@
-import java.util.HashMap;
-import java.util.HashSet;
-
 class Main {
     public static void main(String[] args) {
-
-        String a = "a, a, a, a, b,b,b,c, c";
-
-        String[] b = new String[1];
-        b[0] = "a";
-
         Solution sl = new Solution();
-        String res = sl.mostCommonWord(a, b);
-        System.out.println("res " + res);
+
+        String s = "babad";
+        String res = sl.longestPalindrome(s);
+
+        System.out.println(res);
     }
 }
 
 class Solution {
-    public String mostCommonWord(String paragraph, String[] banned) {
-        // 1. 정규 표현식으로 영문자가 아닌 문자는 공백1칸으로 변경
-        paragraph = paragraph.replaceAll("[^a-zA-Z]", " ");
-
-        // 2. 한칸 이상의 공백을 기준으로 문자열 분리
-        String[] p = paragraph.toLowerCase().split(" +");
-
-        // 3. 금지된 단어를 저장할 set
-        HashSet<String> s = new HashSet<String>();
-        for(String word: banned) {
-            s.add(word);
-        }
-
-        // 4. 개수를 검사할 map
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
+    public String longestPalindrome(String s) {
 
         String result = "";
-        int cnt = 0;
+        int size = s.length();
 
-        for(String word: p) {
+        // 1. 에외 처리
+        if(size < 2 || s.equals(reverString(s))) return s;
 
-            if(s.contains(word)) continue;
-
-            if(!map.containsKey(word)) {
-                map.put(word, 1);
-            }
-            else {
-                int cur_cnt = map.get(word);
-                map.put(word, cur_cnt + 1);
-            }
-
-            if(map.get(word) > cnt) {
-                result = word;
-                cnt = map.get(word);
-            }
+        // 2. 중심점 하나씩 탐색
+        for(int i=0; i<=size-1; i++) {
+            result = maxString3(result,
+                    // 3. 투 포인터 확장
+                    expand(i, i+1, s),
+                    expand(i, i+2, s)
+            );
         }
 
         return result;
+    }
+
+    public String expand(int l, int r, String s) {
+
+        while (l >= 0 && r <= s.length() && s.charAt(l) == s.charAt(r-1)) {
+            l--;
+            r++;
+        }
+        return s.substring(l+1, r-1);
+    }
+
+    public String maxString(String a, String b) {
+        return a.length() > b.length() ? a : b;
+    }
+
+    public String maxString3(String a, String b, String c) {
+        return maxString(maxString(a, b), c);
+    }
+
+    public String reverString(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        return sb.reverse().toString();
     }
 }
