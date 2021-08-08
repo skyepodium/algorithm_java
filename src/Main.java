4dd4d4193e194eabf935467c692f74e4caaae315
skyepodium <skyepodium@gmail.com>
2021-08-08 21:14:54 +0900

    trapping-rain-water

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,39 @@ class Main {
     public static void main(String[] args) {
         Solution sl = new Solution();
 
-        int[] nums = {1, 2, 3, 4};
-        int[] res = sl.productExceptSelf(nums);
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
 
-        for(int num: res) {
-            System.out.println(num);
-        }
+        int res = sl.trap(height);
+
+        System.out.println(res);
     }
 }
 
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
+    public int trap(int[] height) {
 
-        int size = nums.length;
-        int[] leftTimes = new int[size];
-        int[] rightTimes = new int[size];
-        int[] result = new int[size];
+        int result = 0;
 
-        // 1. 초기값 설정
-        leftTimes[0] = nums[0];
-        rightTimes[size-1] = nums[size-1];
+        // 1. 왼쪽, 오른쪽 최대 높이를 저장할 변수 생성
+        int l = 0;
+        int r = height.length - 1;
+        int leftMax = height[l];
+        int rightMax = height[r];
 
-        // 2. 왼쪽 누적곱을 만든다.
-        for(int i=1; i<size; i++) {
-            leftTimes[i] = leftTimes[i-1] * nums[i];
-        }
+        while(l <= r) {
+            // 2. 투 포인터 왼쪽, 오른쪽 최대값 갱신
+            leftMax = Math.max(leftMax, height[l]);
+            rightMax = Math.max(rightMax, height[r]);
 
-        // 3. 오른쪽 누접곱을 만든다.
-        for(int i=size-2; i>=0; i--) {
-            rightTimes[i] = rightTimes[i+1] * nums[i];
-        }
-
-        // 4. 자신을 제외한 양옆의 곱을 계산한다.
-        for(int i=0; i<size; i++) {
-            int leftVal = i-1 >= 0 ? leftTimes[i-1] : 1;
-            int rightVal = i+1 <= size-1 ? rightTimes[i+1] : 1;
-            result[i] = leftVal * rightVal;
+            // 3. 최대 높이를 향해 투 포인터 이동
+            if(leftMax <= rightMax) {
+                result += leftMax - height[l];
+                l++;
+            }
+            else {
+                result += rightMax - height[r];
+                r--;
+            }
         }
 
         return result;
