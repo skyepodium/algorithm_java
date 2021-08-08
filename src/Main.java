5c1065aa366b4545a7d58d5b0caac757c8d8c1ea
skyepodium <skyepodium@gmail.com>
2021-08-08 16:46:22 +0900

    product-of-array-except-self/

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,45 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
 class Main {
     public static void main(String[] args) {
         Solution sl = new Solution();
-        String[] logs = {"dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"};
-        String[] res = sl.reorderLogFiles(logs);
 
-        for(String word: res) {
-            System.out.println(word);
+        int[] nums = {1, 2, 3, 4};
+        int[] res = sl.productExceptSelf(nums);
+
+        for(int num: res) {
+            System.out.println(num);
         }
     }
 }
 
 class Solution {
-    public String[] reorderLogFiles(String[] logs) {
-        ArrayList<String> letters = new ArrayList<>();
-        ArrayList<String> digits = new ArrayList<>();
-         for(String log: logs) {
-            String[] splitLog = log.split(" ");
+    public int[] productExceptSelf(int[] nums) {
 
-            if(isDigit(splitLog[1])) digits.add(log);
-            else letters.add(log);
+        int size = nums.length;
+        int[] leftTimes = new int[size];
+        int[] rightTimes = new int[size];
+        int[] result = new int[size];
+
+        // 1. 초기값 설정
+        leftTimes[0] = nums[0];
+        rightTimes[size-1] = nums[size-1];
+
+        // 2. 왼쪽 누적곱을 만든다.
+        for(int i=1; i<size; i++) {
+            leftTimes[i] = leftTimes[i-1] * nums[i];
         }
 
-         // 3. letters 사전 순 정렬, 만약 같으면 identifier 사전 순 정렬
-        Collections.sort(letters, (a, b) -> {
-            String first = a.split(" ", 2)[1];
-            String second = b.split(" ", 2)[1];
+        // 3. 오른쪽 누접곱을 만든다.
+        for(int i=size-2; i>=0; i--) {
+            rightTimes[i] = rightTimes[i+1] * nums[i];
+        }
 
-            if(first.equals(second)) {
-                return a.split(" ")[0].compareTo(b.split(" ")[0]);
-            }
-            else{
-                return first.compareTo(second);
-            }
-        });
-
-        ArrayList<String> result = new ArrayList<>();
-        result.addAll(letters);
-        result.addAll(digits);
-        String[] arr = new String[result.size()];
-
-        return result.toArray(arr);
-    }
-
-    public Boolean isDigit(String s) {
-        int size = s.length();
+        // 4. 자신을 제외한 양옆의 곱을 계산한다.
         for(int i=0; i<size; i++) {
-            if(!Character.isDigit(s.charAt(i))) return false;
+            int leftVal = i-1 >= 0 ? leftTimes[i-1] : 1;
+            int rightVal = i+1 <= size-1 ? rightTimes[i+1] : 1;
+            result[i] = leftVal * rightVal;
         }
-        return true;
+
+        return result;
     }
 }
