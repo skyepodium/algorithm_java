e7d4bb059710524907c786713716d82402e97f84
skyepodium <skyepodium@gmail.com>
2021-08-09 11:48:22 +0900

    valid-parentheses

--- src/Main.java
+++ src/Main.java
@@ -11,16 +11,15 @@ class Solution {
 
         ArrayList<Integer> result = new ArrayList<>();
 
-        ListNode node = head;
-
-        while(node != null) {
-            result.add(node.val);
-            node = node.next;
+        // 1. 연결 리스트를 모두 꺼내서 리스트에 넣어줍니다.
+        while(head != null) {
+            result.add(head.val);
+            head = head.next;
         }
 
+        // 2. 투 포인터를 사용해서 팰린드롬 여부를 반환합니다.
         int l = 0;
         int r = result.size() - 1;
-
         while(l < r) {
             if(result.get(l) != result.get(r)) return false;
             l++;
