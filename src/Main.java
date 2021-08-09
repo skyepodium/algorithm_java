557f51a900bcc50aadc6d401259b626c92a1f478
skyepodium <skyepodium@gmail.com>
2021-08-09 12:03:02 +0900

    valid-parentheses

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,57 @@
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Stack;
 
 class Main {
     public static void main(String[] args) {
+        Solution sl = new Solution();
 
+        String s = "()[]{}";
+
+        Boolean res = sl.isValid(s);
+
+        System.out.println(res);
     }
 }
 
 class Solution {
-    public boolean isPalindrome(ListNode head) {
+    public boolean isValid(String s) {
 
-        ArrayList<Integer> result = new ArrayList<>();
+        // 1. 여는 괄호 기준으로 닫는 괄호 map 생성
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put('{', '}');
+        map.put('[', ']');
+        map.put('(', ')');
 
-        // 1. 연결 리스트를 모두 꺼내서 리스트에 넣어줍니다.
-        while(head != null) {
-            result.add(head.val);
-            head = head.next;
+        // 2. 스택 생성
+        Stack<Character> stack = new Stack<>();
+
+        int size = s.length();
+        for(int i=0; i<size; i++) {
+            char cur = s.charAt(i);
+
+            // 3. 스택이 들어있는 경우
+            if(!stack.empty()) {
+
+                // 현재가 시작 괄호이면 push
+                if(map.get(cur) != null) {
+                    stack.add(cur);
+                }
+                // 현재가 닫는 과로인데 top의 닫는 괄호가 아니면 종료
+                else {
+                    if(map.get(stack.pop()) != cur) return false;
+                }
+            }
+            // 4. 스택이 비어있는 경우
+            else{
+                stack.add(cur);
+                // 현재가 닫는 괄호이면 종료
+                if(map.get(cur) == null) return false;
+            }
         }
 
-        // 2. 투 포인터를 사용해서 팰린드롬 여부를 반환합니다.
-        int l = 0;
-        int r = result.size() - 1;
-        while(l < r) {
-            if(result.get(l) != result.get(r)) return false;
-            l++;
-            r--;
-        }
+        // 5. 여는 괄호가 하나라도 남아있으면 종료
+        if(!stack.empty()) return false;
 
         return true;
     }
-}
-
-class ListNode {
-     int val;
-     ListNode next;
-     ListNode() {}
-     ListNode(int val) { this.val = val; }
-     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
