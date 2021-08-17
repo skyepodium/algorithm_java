c19b0795dab52ed1964690b72d71ef76a24bb798
skyepodium <skyepodium@gmail.com>
2021-08-18 00:03:12 +0900

    two-sum-ii-input-array-is-sorted

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,27 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         // 1. init
-        int l = 0;
-        int r = numbers.length - 1;
+        int size = numbers.length;
         int[] answer= {0, 0};
 
-        // 2. two pointer
-        while(l <= r) {
-            int sumVal = numbers[l] + numbers[r];
+        for(int i=0; i<size-1; i++) {
+            int l = i+1;
+            int r = size-1;
+            int remain = target - numbers[i];
 
-            if(sumVal < target) {
-                l++;
-            }
-            else if(sumVal > target) {
-                r--;
-            }
-            else {
-                answer[0] = l+1;
-                answer[1] = r+1;
-                return answer;
+            while(l<=r) {
+                int mid = l + (r-l) / 2;
+                if(numbers[mid] < remain) {
+                    l = mid + 1;
+                }
+                else if(numbers[mid] > remain) {
+                    r = mid - 1;
+                }
+                else {
+                    answer[0] = i+1;
+                    answer[1] = mid + 1;
+                    return answer;
+                }
             }
         }
         return answer;
