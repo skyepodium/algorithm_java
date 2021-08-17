8a37d602e1f9fc01d27ba66ae7556a92bc93b4fb
skyepodium <skyepodium@gmail.com>
2021-08-17 21:04:37 +0900

    leet code binary search

--- src/Main.java
+++ src/Main.java
@@ -1,87 +1,31 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
 
-class Main {
-    public static void main(String[] args) {
-//        String numbers = "17";
-        String numbers = "011";
+        return binarySearch(l,r, nums, target);
+    }
 
-        Solution sl = new Solution();
+    public int binarySearch(int l, int r, int[] nums, int target) {
+        while(l <= r) {
+            int mid = (l+r) / 2;
 
-        int res = sl.solution(numbers);
-
-        System.out.println("res " + res);
-
+            if(nums[mid] < target) {
+                l++;
+            }
+            else if(nums[mid] > target) {
+                r--;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
 
-class Solution {
-    int size;
-    boolean[] d, check, used;
-    Integer[] nums;
-    ArrayList<Integer> stack = new ArrayList<>();
-    int cnt = 0;
-    public int solution(String numbers) {
-        // 1. init
-        size = numbers.length();
+class Main {
+    public static void main(String[] args) {
 
-        nums = new Integer[size];
-        for(int i=0; i<size; i++) {
-            nums[i] = Integer.parseInt(String.valueOf(numbers.charAt(i)));
-        }
-        Arrays.sort(nums, Collections.reverseOrder());
-
-        int maxInt = 0;
-        for(int num: nums){
-            maxInt = maxInt * 10 + num;
-        }
-        d = new boolean[maxInt + 1];
-        used = new boolean[maxInt + 1];
-
-        // 2. eratos
-        eratos(maxInt);
-
-        int answer = 0;
-
-        check = new boolean[size];
-        dfs(0);
-
-        return cnt;
-    }
-
-    public void dfs(int idx) {
-        if(stack.size() >= 1 && !(stack.size() >= 2 && stack.get(0) == 0)) {
-            Integer val = stack.stream().reduce(0, (a, b) -> {
-                return a * 10 + b;
-            });
-
-            if(val >= 2 && !used[val] && !d[val]) {
-                used[val] = true;
-                cnt++;
-            }
-        }
-
-        if(idx == size) {
-            return;
-        }
-
-        for(int i=0; i<size; i++) {
-            if(!check[i]) {
-                check[i] = true;
-                stack.add(nums[i]);
-                dfs(idx + 1);
-                check[i] = false;
-                stack.remove(stack.size()-1);
-            }
-        }
-    }
-
-    public void eratos(int n) {
-        for(int i=2; i*i<=n; i++) {
-            for(int j=i+i; j<=n; j=j+i) {
-                if(!d[j]) d[j] = true;
-            }
-        }
     }
 }
