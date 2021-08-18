38e6708c9edabfd7fff83d99979da5e4855347a5
skyepodium <skyepodium@gmail.com>
2021-08-18 20:46:04 +0900

    binary search

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,63 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         // 1. init
-        int n = matrix.length - 1;
-        int m = matrix[0].length - 1;
-        int i = 0;
-        int j = m;
+        int n = matrix.length;
+        int m = matrix[0].length;
 
-        // 2. loop
-        while (i<=n && j>=0) {
-            int cur = matrix[i][j];
+        // 2. verticalBinarySearch
+        int vIdx = verticalBinarySearch(0, n-1, matrix, target);
 
-            if(cur > target) {
-                j--;
+        if (vIdx < 0) return false;
+
+        // 3. horizontalBinarySearch
+        return horizontalBinarySearch(0, m-1, matrix, target, vIdx);
+    }
+
+    public boolean horizontalBinarySearch(int l, int r, int[][] matrix, int target, int vIdx) {
+        while(l <= r) {
+            int mid = l + (r-l) / 2;
+            int cur = matrix[vIdx][mid];
+
+            if(cur < target) {
+                l = mid + 1;
             }
-            else if (cur < target) {
-                i++;
+            else if(cur > target) {
+                r = mid - 1;
             }
-            else {
+            else{
                 return true;
             }
         }
         return false;
     }
+
+    public int verticalBinarySearch(int l, int r, int[][] matrix, int target) {
+
+        while (l < r) {
+            int mid = l + (r-l) / 2;
+            int cur = matrix[mid][0];
+
+            if(cur < target) {
+                l = mid + 1;
+            }
+            else {
+                r = mid;
+            }
+        }
+        return matrix[r][0] > target ? r - 1 : r;
+    }
 }
 
 class Main {
     public static void main(String[] args) {
+        Solution sl = new Solution();
+
+        int[][] matrix = {{1}};
+        int target = 0;
+
+        boolean res = sl.searchMatrix(matrix, target);
+
+        System.out.println("res " + res);
 
     }
 }
