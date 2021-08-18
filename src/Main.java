124d6fc5951098a67f11cdf28e4bfe1daee69d14
skyepodium <skyepodium@gmail.com>
2021-08-18 18:51:29 +0900

    search-a-2d-matrix

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,31 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 class Solution {
-    public int[] intersection(int[] nums1, int[] nums2) {
+    public boolean searchMatrix(int[][] matrix, int target) {
         // 1. init
-        int size1 = nums1.length;
-        int size2 = nums2.length;
-        ArrayList<Integer> res = new ArrayList<>();
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        int n = matrix.length - 1;
+        int m = matrix[0].length - 1;
+        int i = 0;
+        int j = m;
 
-        for(int i=0; i<size1; i++) {
-            if(i >= 1 && nums1[i] == nums1[i-1]) {
-                continue;
+        // 2. loop
+        while (i<=n && j>=0) {
+            int cur = matrix[i][j];
+
+            if(cur > target) {
+                j--;
             }
-
-            int val = nums1[i];
-
-            if(binarySearch(0, size2-1, nums2, val) != -1) {
-                res.add(val);
+            else if (cur < target) {
+                i++;
+            }
+            else {
+                return true;
             }
         }
-        int resSize = res.size();
-        int[] result = new int[resSize];
-        for(int i=0; i<resSize; i++) {
-            result[i] = res.get(i);
-        }
-
-        return result;
+        return false;
     }
+}
 
-    public int binarySearch(int l, int r, int[] nums, int target) {
-        while(l <= r) {
-            int mid = l + (r-l) / 2;
+class Main {
+    public static void main(String[] args) {
 
-            if(nums[mid] < target) {
-                l = mid + 1;
-            }
-            else if(nums[mid] > target) {
-                r = mid - 1;
-            }
-            else{
-                return mid;
-            }
-        }
-        return -1;
     }
 }
