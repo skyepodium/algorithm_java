3ebad83e81315b7c66ea31f91e35ee0fade3b8f7
skyepodium <skyepodium@gmail.com>
2021-08-19 23:24:46 +0900

    squares-of-a-sorted-array

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,14 @@
+import java.util.Arrays;
+
 class Solution {
+    public int[] sortedSquares(int[] nums) {
 
-    int[] d;
-    int res = 0;
-    int size = 0;
-    public int rob(int[] nums) {
-        // 1. init
-        size = nums.length;
-        d = new int[size];
-
-        // 2. exception
-        if(size <= 2) {
-            return max(nums);
+        for(int i=0; i<nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
         }
 
-        // 3. tabulation
-        d[0] = nums[0];
-        d[1] = max(nums[0], nums[1]);
-        for(int i=2; i<size; i++) {
-            d[i] = max(d[i-2] + nums[i], d[i-1]);
-        }
+        Arrays.sort(nums);
 
-        return d[size-1];
-    }
-
-    public int max(int[] arr) {
-        res = 0;
-        for(int num: arr) {
-            res = max(res, num);
-        }
-        return res;
-    }
-
-    public int max(int a, int b){
-        return a > b ? a : b;
+        return nums;
     }
 }
