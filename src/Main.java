edd625f9ba2d280eb8bb732a0a5d1168db2c05ae
skyepodium <skyepodium@gmail.com>
2021-08-20 00:59:21 +0900

    merge-two-sorted-lists

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
-import java.util.Arrays;
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public int[] sortedSquares(int[] nums) {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
-        for(int i=0; i<nums.length; i++) {
-            nums[i] = nums[i] * nums[i];
+        if((l1 == null) || (l2 != null && l1.val > l2.val)) {
+            ListNode temp = new ListNode();
+            temp = l1;
+            l1 = l2;
+            l2 = temp;
         }
 
-        Arrays.sort(nums);
+        if(l1 != null){
+            l1.next = this.mergeTwoLists(l1.next, l2);
+        }
 
-        return nums;
+        return l1;
     }
 }
