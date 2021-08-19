6c15021257ed4c1c2ffee14b96f371affd92149f
skyepodium <skyepodium@gmail.com>
2021-08-19 22:38:02 +0900

    leet code house-robber

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,37 @@
 class Solution {
-    int d[];
-    public int climbStairs(int n) {
-        d = new int[n+1];
 
-        return go(n);
+    int[] d;
+    int res = 0;
+    int size = 0;
+    public int rob(int[] nums) {
+        // 1. init
+        size = nums.length;
+        d = new int[size];
+
+        // 2. exception
+        if(size <= 2) {
+            return max(nums);
+        }
+
+        // 3. tabulation
+        d[0] = nums[0];
+        d[1] = max(nums[0], nums[1]);
+        for(int i=2; i<size; i++) {
+            d[i] = max(d[i-2] + nums[i], d[i-1]);
+        }
+
+        return d[size-1];
     }
 
-    public int go(int i) {
-        if(i <= 2) return i;
+    public int max(int[] arr) {
+        res = 0;
+        for(int num: arr) {
+            res = max(res, num);
+        }
+        return res;
+    }
 
-        if(d[i] > 0) return d[i];
-
-        d[i] = go(i-2) + go(i-1);
-
-        return d[i];
+    public int max(int a, int b){
+        return a > b ? a : b;
     }
 }
