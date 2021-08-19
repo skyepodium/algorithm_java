cc23d0be70ab9e13ca0ec7bc48b9c290cee00ba2
skyepodium <skyepodium@gmail.com>
2021-08-19 21:59:19 +0900

    leet code maximum-subarray memoization

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,33 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
+
+    int size;
+    int[] d;
+    int res;
+
+    public int maxSubArray(int[] nums) {
         // 1. init
-        int n = matrix.length;
-        int m = matrix[0].length;
+        size = nums.length;
+        d = new int[size];
+        res = nums[0];
 
-        // 2. verticalBinarySearch
-        int vIdx = verticalBinarySearch(0, n-1, matrix, target);
+        go(size-1, nums);
 
-        if (vIdx < 0) return false;
-
-        // 3. horizontalBinarySearch
-        return horizontalBinarySearch(0, m-1, matrix, target, vIdx);
+        return res;
     }
 
-    public boolean horizontalBinarySearch(int l, int r, int[][] matrix, int target, int vIdx) {
-        while(l <= r) {
-            int mid = l + (r-l) / 2;
-            int cur = matrix[vIdx][mid];
+    public int go(int idx, int[] nums) {
+        if(idx < 1) return nums[0];
 
-            if(cur < target) {
-                l = mid + 1;
-            }
-            else if(cur > target) {
-                r = mid - 1;
-            }
-            else{
-                return true;
-            }
-        }
-        return false;
+        if(d[idx] > 0) return d[idx];
+
+        d[idx] = max(nums[idx], go(idx-1, nums) + nums[idx]);
+
+        res = max(res, d[idx]);
+
+        return d[idx];
     }
 
-    public int verticalBinarySearch(int l, int r, int[][] matrix, int target) {
-
-        while (l < r) {
-            int mid = l + (r-l) / 2;
-            int cur = matrix[mid][0];
-
-            if(cur < target) {
-                l = mid + 1;
-            }
-            else {
-                r = mid;
-            }
-        }
-        return matrix[r][0] > target ? r - 1 : r;
-    }
-}
-
-class Main {
-    public static void main(String[] args) {
-        Solution sl = new Solution();
-
-        int[][] matrix = {{1}};
-        int target = 0;
-
-        boolean res = sl.searchMatrix(matrix, target);
-
-        System.out.println("res " + res);
-
+    public int max(int a, int b) {
+        return a > b ? a : b;
     }
 }
