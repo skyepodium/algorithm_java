1538dbf95c0160f1b218fb5a46a401f94ec42c12
skyepodium <skyepodium@gmail.com>
2021-08-22 12:41:39 +0900

    remove-duplicate-letters

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,51 @@
+import java.util.HashSet;
 import java.util.Stack;
 
 class Solution {
-    public int[] dailyTemperatures(int[] temperatures) {
+    public String removeDuplicateLetters(String s) {
         // 1. init
-        int size = temperatures.length;
-        int[] res = new int[size];
-        Stack<Integer> s = new Stack<>();
+        Stack<Character> st = new Stack<>();
+        HashSet<Character> seen = new HashSet<>();
+        int[] counter = new int[26];
+        int size = s.length();
 
-        // 2. loop
         for(int i=0; i<size; i++) {
-            int cur = temperatures[i];
-            while(!s.empty() && temperatures[s.peek()] < cur) {
-                int top = s.pop();
-                res[top] = i - top;
+            char c = s.charAt(i);
+            int order = (int)c - (int)'a';
+            counter[order]++;
+        }
+
+        for(int i=0; i<size; i++) {
+            char c = s.charAt(i);
+            int order = (int)c - (int)'a';
+            counter[order]--;
+
+            if(seen.contains(c)) continue;
+
+            while(!st.empty() && st.peek() > c && counter[(int)st.peek() - (int)'a'] > 0) {
+                seen.remove(st.pop());
             }
 
-            s.add(i);
+            seen.add(c);
+            st.add(c);
+        }
+
+        String res = "";
+        while(!st.empty()) {
+            res = st.pop() + res;
         }
 
         return res;
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        Solution sl = new Solution();
+
+        String s = "bcabc";
+        String res = sl.removeDuplicateLetters(s);
+
+        System.out.println("res " + res);
     }
 }
