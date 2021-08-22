d8e55bb0de1539b97d5bab2925d0c022e50b7962
skyepodium <skyepodium@gmail.com>
2021-08-22 23:09:56 +0900

    linked-list-cycle

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,16 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode removeElements(ListNode head, int val) {
-        ListNode root = new ListNode(0, null);
-        ListNode node = root;
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
 
-        while(head != null) {
-            if(head.val != val) {
-                node.next = new ListNode(head.val, null);
-                node = node.next;
+        while(fast != null && fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow) {
+                return true;
             }
-
-            head = head.next;
         }
-
-        return root.next;
+        return false;
     }
-}
-
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
