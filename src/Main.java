623b8b6240375e3764918d8514c39134d0318bc0
skyepodium <skyepodium@gmail.com>
2021-08-28 22:19:53 +0900

    remove-duplicates-from-sorted-list

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,17 @@
 class Solution {
-    int res = 100001;
-    public int minDepth(TreeNode root) {
-        // 1. exception
-        if(root == null) return 0;
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode root = head;
 
-        // 2. dfs
-        dfs(root, 1);
-
-        return res;
-    }
-
-    public void dfs(TreeNode root, int cnt) {
-        if(root == null) return;
-
-        if(root.left == null && root.right == null) {
-            res = Math.min(res, cnt);
+        while(head != null) {
+            if(head.next != null && head.val == head.next.val) {
+                head.next = head.next.next;
+            }
+            else{
+                head = head.next;
+            }
         }
 
-        dfs(root.left, cnt + 1);
-        dfs(root.right, cnt + 1);
 
+        return root;
     }
 }
