65c828113f006398f94568219676666fbd612f47
skyepodium <skyepodium@gmail.com>
2021-08-30 21:50:51 +0900

    invert-binary-tree

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
 class Solution {
-    public int solution(String skill, String[] skill_trees) {
-        // 1. init
-        String pattern = "[^"+skill+"]";
-        int res = 0;
-
-        // 2. loop
-        for(String s: skill_trees) {
-            String r = s.replaceAll(pattern, "");
-
-            if(skill.indexOf(r) == 0) res++;
+    public TreeNode invertTree(TreeNode root) {
+        if(root != null) {
+            TreeNode temp = null;
+            temp = root.left;
+            root.left = invertTree(root.right);
+            root.right = invertTree(temp);
         }
 
-        return res;
+        return root;
     }
 }
