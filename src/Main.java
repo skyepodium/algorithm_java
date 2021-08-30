9679734762a13ae9c98ce69ee30d33da17a5318e
skyepodium <skyepodium@gmail.com>
2021-08-30 18:19:58 +0900

    programmers 구명보트

--- src/Main.java
+++ src/Main.java
@@ -1,88 +1,25 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 
 class Solution {
-    public String[] solution(String[] files) {
+    public int solution(int[] people, int limit) {
         // 1. init
-        List<Info> fList =  splitFile(files);
-        int size = files.length;
-        String[] res = new String[size];
+        int l = 0;
+        int r = people.length - 1;
+        int res = 0;
 
         // 2. sort
-        fList = fList.stream().sorted((a, b) -> {
-            if (a.head.toLowerCase().equals(b.head.toLowerCase())) {
-                if(a.number == b.number) {
-                    return a.idx - b.idx;
-                }
-                else {
-                    return a.number - b.number;
-                }
+        Arrays.sort(people);
+
+        // 3. two pointer
+        while(l <= r) {
+            res++;
+
+            if(people[l] + people[r] <= limit) {
+                l++;
             }
-            return a.head.toLowerCase().compareTo(b.head.toLowerCase());
-        }).collect(Collectors.toList());
-
-        // 3. loop
-        for(int i=0; i<size; i++) {
-            res[i] = fList.get(i).file;
+            r--;
         }
 
         return res;
-    }
-
-    public ArrayList<Info> splitFile(String[] files) {
-        ArrayList<Info> res = new ArrayList<>();
-
-        int idx = 0;
-        for(String s: files) {
-            // 1. head
-            Pattern headPattern = Pattern.compile("[^0-9]+");
-            Matcher headMatch = headPattern.matcher(s);
-            String head = "";
-            if(headMatch.find()) head = headMatch.group(0);
-
-            // 2. number
-            Pattern numberPatther = Pattern.compile("\\d{1,5}");
-            Matcher numberMatch = numberPatther.matcher(s);
-            String strNumber = "";
-            if(numberMatch.find()) strNumber = numberMatch.group(0);
-            int number = Integer.parseInt(strNumber);
-
-            // 3. add
-            res.add(new Info(head, number, idx, s));
-        }
-
-        return res;
-    }
-}
-
-class Info {
-    String head;
-    int number;
-    int idx;
-    String file;
-
-    public Info(String head, int number, int idx, String file) {
-        this.head = head;
-        this.number = number;
-        this.idx = idx;
-        this.file = file;
-    }
-}
-
-class Main {
-    public static void main(String[] args) {
-        String[] files = {"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"};
-
-        Solution sl = new Solution();
-
-        String[] res = sl.solution(files);
-
-        for(String s: res) {
-            System.out.println(s);
-        }
     }
 }
