6320a261c2a8fb6b3e3bff16d0b3067a3b20e837
skyepodium <skyepodium@gmail.com>
2021-09-05 12:57:29 +0900

    binary-tree-preorder-traversal

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,21 @@ import java.util.ArrayList;
 
 class Solution {
     List<Integer> res = new ArrayList<>();
-    public List<Integer> inorderTraversal(TreeNode root) {
+    public List<Integer> preorderTraversal(TreeNode root) {
         // 1. exception
         if(root == null) return res;
 
-        // 2. inorder
-        inOrder(root);
+        // 2. preOrder
+        preOrder(root);
 
         return res;
     }
 
-    public void inOrder(TreeNode node) {
+    public void preOrder(TreeNode node) {
         if(node == null) return;
 
-        inOrder(node.left);
         res.add(node.val);
-        inOrder(node.right);
+        preOrder(node.left);
+        preOrder(node.right);
     }
 }
