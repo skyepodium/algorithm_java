6a52a77a7a77d31380aef8484c6622d9e82b00b3
skyepodium <skyepodium@gmail.com>
2021-09-10 23:33:40 +0900

    remove-element

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
+    public int removeElement(int[] nums, int val) {
         int cnt = 0;
         int n = nums.length;
 
-        for(int i=1; i<n; i++) {
-            if(nums[i] == nums[i-1]) cnt++;
+        for(int i=0; i<n; i++) {
+            if(nums[i] == val) cnt++;
             else nums[i-cnt] = nums[i];
         }
+
         return n - cnt;
     }
 }
