a0d669eabbb7126ac2a8c8d17f7d6f93ffebdbbe
skyepodium <skyepodium@gmail.com>
2021-09-26 22:55:30 +0900

    delete-node-in-a-linked-list

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,6 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        // 1. init
-        int res = nums[0];
-        int size = nums.length;
-
-        // 2. xor
-        for(int i=1; i<size; i++) {
-            res ^= nums[i];
-        }
-
-        return res;
-    }
-}
-
-class Main {
-    public static void main(String[] args) {
-        Solution sl = new Solution();
-        int[] nums = {2, 2, 1};
-        int res = sl.singleNumber(nums);
-
-        System.out.println("res " + res);
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 }
