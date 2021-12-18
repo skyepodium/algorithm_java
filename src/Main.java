9430cf2f428cf47958462c6279709b7009d3d9a0
skyepodium <skyepodium@gmail.com>
2021-12-18 16:40:06 +0900

    contains-duplicate

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,24 @@
 import java.util.Arrays;
-import java.util.stream.Collectors;
-
-class Solution {
-    public String solution(String s) {
-        String answer = "";
-
-        boolean isFirst = true;
-        for(int i=0; i<s.length(); i++) {
-            String a  = s.charAt(i) + "";
-            if(a.equals(" ")) {
-                isFirst = true;
-            }
-            else {
-                if(isFirst) {
-                    isFirst = false;
-                    a = a.toUpperCase();
-                }
-                else {
-                    a = a.toLowerCase();
-                }
-            }
-            answer += a;
-        }
-
-        return answer;
-    }
-}
+import java.util.HashSet;
+import java.util.Set;
 
 class Main {
     public static void main(String[] args) {
-        Solution sl = new Solution();
-        String res = sl.solution("3people unFollowed me");
 
-        System.out.println("res " + res);
+
+
+    }
+}
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+
+        for(int x: nums) {
+            if(!s.contains(x)) s.add(x);
+            else return true;
+        }
+
+        return false;
     }
 }
