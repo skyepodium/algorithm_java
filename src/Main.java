65cd19586f4a755a87989f3b30056d735aedf290
skyepodium <skyepodium@gmail.com>
2021-12-18 09:57:01 +0900

    JadenCase 문자열 만들기

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,28 @@
-/*
-시간 복잡도: O(n)
-공간 복잡도: O(n)
-사용한 알고리즘: 반복문
-사용한 자료구조: 트리
- */
-
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 class Solution {
+    public String solution(String s) {
+        String answer = "";
 
-    public static int n, m, idx;
-    public static HashMap<String, String> p = new HashMap<>();
-    public static HashMap<String, Integer> idxMap = new HashMap<>();
-
-    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        // 1. init
-        n = enroll.length;
-        m = seller.length;
-        int[] answer = new int[n];
-
-        // 2. make tree
-        for(int i=0; i<n; i++) {
-            String e = enroll[i];
-            String r = referral[i];
-            p.put(e, r);
-            idxMap.put(e, idx);
-            idx++;
-        }
-
-        // 3. loop
-        for(int i=0; i<m; i++) {
-            String s = seller[i];
-            int a = amount[i] * 100;
-            String cur = s;
-
-            float remain = a;
-            while(remain > 0 && !cur.equals("-")) {
-                // 1) check
-                int curIdx = idxMap.get(cur);
-
-                // 2) calculate
-                int upper = (int)(remain * 0.1);
-                int mine = (int)remain - upper;
-
-                if(upper < 1) {
-                    mine = (int)remain;
-                    upper = 0;
-                }
-
-                // 3) update
-                answer[curIdx] += mine;
-                cur = p.get(cur);
-                remain = upper;
+        boolean isFirst = true;
+        for(int i=0; i<s.length(); i++) {
+            String a  = s.charAt(i) + "";
+            if(a.equals(" ")) {
+                isFirst = true;
             }
+            else {
+                if(isFirst) {
+                    isFirst = false;
+                    a = a.toUpperCase();
+                }
+                else {
+                    a = a.toLowerCase();
+                }
+            }
+            answer += a;
         }
+
         return answer;
     }
 }
@@ -61,6 +30,8 @@ class Solution {
 class Main {
     public static void main(String[] args) {
         Solution sl = new Solution();
+        String res = sl.solution("3people unFollowed me");
 
+        System.out.println("res " + res);
     }
 }
