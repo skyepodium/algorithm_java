fcbeb6a9f245b3a838167e1f05cff8b8b84a6723
skyepodium <skyepodium@gmail.com>
2021-12-20 01:02:04 +0900

    leet code xor-operation-in-an-array

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,9 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public int maxDiameter = 0;
-    public int diameterOfBinaryTree(TreeNode root) {
-
-        go(root);
-
-        return maxDiameter;
-    }
-
-    public int go(TreeNode node) {
-        if(node == null) return 0;
-
-        int leftDepth = go(node.left);
-        int rightDepth = go(node.right);
-
-        this.maxDiameter = Math.max(this.maxDiameter, leftDepth + rightDepth);
-
-        return Math.max(leftDepth, rightDepth) + 1;
+    public int xorOperation(int n, int start) {
+        int result = start;
+        for(int i=start+2; i <= start + 2 * (n-1); i+=2) {
+            result ^= i;
+        }
+        return result;
     }
 }
