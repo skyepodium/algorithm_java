d45f687cef867118f3bbdae192cb0227c20d0093
skyepodium <skyepodium@gmail.com>
2021-12-25 23:08:40 +0900

    effective java - equals hashCode

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,51 @@
+import java.util.HashMap;
+import java.util.Objects;
+
 class Main {
-
     public static void main(String[] args) {
-        long primitiveStartTime = System.currentTimeMillis();
-        long primitiveSumValue = 0;
-        for(int i=0; i<100000000; i++) {
-            primitiveSumValue += i;
-        }
-        System.out.println("원시 타입 수행 결과: " + (System.currentTimeMillis() - primitiveStartTime) + "ms");
 
-        long referenceStartTime = System.currentTimeMillis();
-        Long referenceSumValue = 0L;
-        for(Integer i=0; i<100000000; i++) {
-            referenceSumValue += i;
-        }
-        System.out.println("참조 타입 수행 결과: " + (System.currentTimeMillis() - referenceStartTime) + "ms");
+        Dog dog1 = new Dog("모카", 1);
+        Dog dog2 = new Dog("모카", 1);
+
+        HashMap<Dog, String> dogHouse = new HashMap<>();
+        dogHouse.put(dog1, "모카집");
+
+        // 모카집 입니다.
+        System.out.println(dogHouse.get(dog1));
+        // null
+        System.out.println(dogHouse.get(dog2));
     }
 }
+
+class Dog {
+    private String name;
+    private int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    // 인텔리제이 generate 기능 사용
+    public boolean equals(Object o) {
+        // 1. 주소값 비교
+        if (this == o) return true;
+
+        // 2. 인스턴스 여부 확인
+        if (!(o instanceof Dog)) return false;
+
+        // 3. 나이, 이름 비교
+        Dog dog = (Dog) o;
+        return age == dog.age && Objects.equals(name, dog.name);
+    }
+
+    @Override
+    // Objects 클래스는 임의의 개수만큼 객체를 받아 해시코드를 계산해줍니다.
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
+
+
+
