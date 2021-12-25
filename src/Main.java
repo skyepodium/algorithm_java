5db8457e042ab943a5ecd166f8c79122fd3b39e6
skyepodium <skyepodium@gmail.com>
2021-12-25 19:51:56 +0900

    primitive vs reference type speed compare

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
-class Solution {
-    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+class Main {
 
-        return String.join("", word1).equals(String.join("", word2));
+    public static void main(String[] args) {
+        long primitiveStartTime = System.currentTimeMillis();
+        long primitiveSumValue = 0;
+        for(int i=0; i<100000000; i++) {
+            primitiveSumValue += i;
+        }
+        System.out.println("원시 타입 수행 결과: " + (System.currentTimeMillis() - primitiveStartTime) + "ms");
+
+        long referenceStartTime = System.currentTimeMillis();
+        Long referenceSumValue = 0L;
+        for(Integer i=0; i<100000000; i++) {
+            referenceSumValue += i;
+        }
+        System.out.println("참조 타입 수행 결과: " + (System.currentTimeMillis() - referenceStartTime) + "ms");
     }
 }
