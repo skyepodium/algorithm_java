044728c2f39b5965af7e468f1ed1f19f099dc394
skyepodium <skyepodium@gmail.com>
2021-12-26 14:57:29 +0900

    excel-sheet-column-number

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,22 @@
+import javax.swing.*;
+
 class Main {
     public static void main(String[] args) {
-        // 생성자를 통해 인스턴스 생성
-        Dog dog = new Dog("모카", 1);
 
-        // 출력
-        System.out.println(dog);
     }
 }
 
-class Dog {
-    // 이름을 저장하는 인스턴스 변수
-    private String name;
-    // 나이를 저장하는 인스턴스 변수
-    private int age;
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        // 1. init
+        int result = 0;
 
-    // 생성자
-    public Dog(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
+        // 2. loop
+        for(int i=0; i<columnTitle.length(); i++) {
+            char c = columnTitle.charAt(i);
+            result = result * 26 + (int)c - (int)'A' + 1;
+        }
 
-    @Override
-    // 인텔리제이의 generate 기능을 사용했습니다.
-    public String toString() {
-        return "Dog{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
+        return result;
     }
 }
