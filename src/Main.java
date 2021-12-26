38bc0358d9e15eda3818d1f3f290ca71fc235edc
skyepodium <skyepodium@gmail.com>
2021-12-26 11:39:13 +0900

    override toString

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,31 @@
-import java.util.HashMap;
-import java.util.Objects;
-
 class Main {
     public static void main(String[] args) {
+        // 생성자를 통해 인스턴스 생성
+        Dog dog = new Dog("모카", 1);
 
-        Dog dog1 = new Dog("모카", 1);
-        Dog dog2 = new Dog("모카", 1);
-
-        HashMap<Dog, String> dogHouse = new HashMap<>();
-        dogHouse.put(dog1, "모카집");
-
-        // 모카집 입니다.
-        System.out.println(dogHouse.get(dog1));
-        // null
-        System.out.println(dogHouse.get(dog2));
+        // 출력
+        System.out.println(dog);
     }
 }
 
 class Dog {
+    // 이름을 저장하는 인스턴스 변수
     private String name;
+    // 나이를 저장하는 인스턴스 변수
     private int age;
 
+    // 생성자
     public Dog(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
     @Override
-    // 인텔리제이 generate 기능 사용
-    public boolean equals(Object o) {
-        // 1. 주소값 비교
-        if (this == o) return true;
-
-        // 2. 인스턴스 여부 확인
-        if (!(o instanceof Dog)) return false;
-
-        // 3. 나이, 이름 비교
-        Dog dog = (Dog) o;
-        return age == dog.age && Objects.equals(name, dog.name);
-    }
-
-    @Override
-    // Objects 클래스는 임의의 개수만큼 객체를 받아 해시코드를 계산해줍니다.
-    public int hashCode() {
-        return Objects.hash(name, age);
+    // 인텔리제이의 generate 기능을 사용했습니다.
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
-
-
-
