e7e7e2c559cbf392bf0ab912b1950c2920bed740
skyepodium <skyepodium@gmail.com>
2022-01-09 21:42:54 +0900

    word-pattern

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,33 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 class Solution {
-    public int findNumbers(int[] nums) {
+    public boolean wordPattern(String pattern, String s) {
         // 1. init
-        int res = 0;
+        String[] sList = s.split(" ");
+        Map<Character, String> m = new HashMap<>();
+        Set<String> e = new HashSet<>();
 
-        // 2. loop
-        for(int i=0; i<nums.length; i++) {
-            int x = nums[i];
+        // 2. exception
+        if(pattern.length() != sList.length) return false;
 
-            int cnt=0;
-            while(x>0) {
-                x /= 10;
-                cnt++;
+        // 3. loop
+        for(int i=0; i<sList.length; i++) {
+            char a = pattern.charAt(i);
+            String b = sList[i];
+
+            if(m.containsKey(a)) {
+                if(!m.get(a).equals(b)) {
+                    return false;
+                }
+            }else{
+                if(e.contains(b)) return false;
+                m.put(a, b);
+                e.add(b);
             }
-            if(cnt%2==0) res++;
         }
-
-        return res;
+        return true;
     }
 }
