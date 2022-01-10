4391e598281a27f69e125d1d7568d166400b9c22
skyepodium <skyepodium@gmail.com>
2022-01-10 22:29:24 +0900

    java tenary operator - Null Pointer Exception

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,10 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+class Main {
+    public static void main(String[] args) {
+        // 1. 참조타입에 null값 대입
+        Long a = null;
 
-class Solution {
-    public boolean wordPattern(String pattern, String s) {
-        // 1. init
-        String[] sList = s.split(" ");
-        Map<Character, String> m = new HashMap<>();
-        Set<String> e = new HashSet<>();
-
-        // 2. exception
-        if(pattern.length() != sList.length) return false;
-
-        // 3. loop
-        for(int i=0; i<sList.length; i++) {
-            char a = pattern.charAt(i);
-            String b = sList[i];
-
-            if(m.containsKey(a)) {
-                if(!m.get(a).equals(b)) {
-                    return false;
-                }
-            }else{
-                if(e.contains(b)) return false;
-                m.put(a, b);
-                e.add(b);
-            }
-        }
-        return true;
+        // 2. 삼항 연산자 수행
+        // 조건이 false 이기 때문에 a가 b에 대입
+        Long b = false ? 0L : a;
     }
 }
