1c422acd30c6df9ee20d89f178c794ad09474b9e
skyepodium <skyepodium@gmail.com>
2022-01-15 00:09:22 +0900

    sum-of-all-odd-length-subarrays

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,26 @@
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.Phaser;
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        // 1. init
+        int n = arr.length;
+        int res = 0;
 
-class Main {
-    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, BrokenBarrierException, InterruptedException {
-
-        //안전하지 않은 객체
-        DateFormat unsafeDateFormat = new SimpleDateFormat("yyyy/MM/dd");
-        Calendar cal1 = Calendar.getInstance();
-        cal1.set(1989,Calendar.MARCH,10);//1989/03/10
-        Date date1 = cal1.getTime();
-        Calendar cal2 = Calendar.getInstance();
-        cal2.set(2020, Calendar.JUNE,20);//2020/06/20
-        Date date2 = cal2.getTime();
-
-        Thread thread1 = new Thread(() ->{
-            for(int i=0;i<100;i++){
-                try{
-                    String result = unsafeDateFormat.format(date1);
-                    System.out.println("Thread1: " + result);
-                }catch(Exception e){
-                    e.printStackTrace();
-                    break;
-                }
+        // 2. loop
+        for(int i=1; i<=n; i=i+2) {
+            for(int j=0; j<n-i+1; j++) {
+                res += getSum(arr, j, j+i-1);
             }
-        });
-        Thread thread2 = new Thread(() ->{
-            for(int i=0;i<100;i++){
-                try{
-                    String result = unsafeDateFormat.format(date2);
-                    System.out.println("Thread2: " + result);
-                }catch(Exception e){
-                    e.printStackTrace();
-                    break;
-                }
-            }
-        });
+        }
 
-        thread1.start();
-        thread2.start();
+        return res;
+    }
+
+    public int getSum(int[] arr, int start, int end) {
+        int res = 0;
+
+        for(int i=start; i<=end; i++) {
+            res += arr[i];
+        }
+
+        return res;
     }
 }
