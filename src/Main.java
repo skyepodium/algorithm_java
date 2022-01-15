6ec56b6b21c8882bfa0f8c4b58fc834a1a444650
skyepodium <skyepodium@gmail.com>
2022-01-15 17:34:29 +0900

    enum 싱글톤

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
+// Main.java
+import java.util.concurrent.*;
 
 class Main {
-    public static void main(String args[]) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        // 1. 싱글톤 클래스의 인스턴스 생성
-        Settings settings = Settings.getInstance();
+    public static void main(String args[]) {
+        // 1. 스레드 풀 생성
+        ExecutorService service = Executors.newCachedThreadPool();
 
-        // 2. 리플랙션
-        // 1) settings 클래스의 생성자 가져옴
-        Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
-        // 2) private 생성자 외부 접근 허용
-        constructor.setAccessible(true);
-        // 3) 생성자를 통한 인스턴스 생성 new Settgings()와 유사
-        Settings settings1 = constructor.newInstance();
-
-        // 3. 주소값 비교 - false
-        System.out.println(settings == settings1);
+        // 2. 반복문을 통해 - 여러개의 스레드가 거의 동시에 인스턴스를 생성
+        for (int i = 0; i < 10; i++) {
+            service.submit(() -> {
+                Settings settings = Settings.INSTANCE;
+                settings.plusCount();
+            });
+        }
+        // 3. 종료
+        service.shutdown();
     }
 }
--- src/Settings.java
+++ src/Settings.java
@@ -1,16 +1,9 @@
-public class Settings {
-    // 1. private 생성자
-    private Settings() {
-        throw new RuntimeException("싱글턴 인스턴스를 리플렉션으로 생성할 수 없습니다.");
-    }
+public enum Settings {
+    INSTANCE;
 
-    // 2. static inner 클래스
-    private static class SettingsHolder {
-        private static final Settings INSTANCE = new Settings();
-    }
+    private int count = 0;
 
-    // 3. static 메서드
-    public static Settings getInstance() {
-        return SettingsHolder.INSTANCE;
+    public void plusCount () {
+        System.out.println(++count);
     }
 }
