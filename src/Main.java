2e1755e542d5c2c9474919824e906cddfa4af565
skyepodium <skyepodium@gmail.com>
2022-01-21 22:48:57 +0900

    StringBuffer thread safe

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,12 @@
-import java.util.ArrayList;
-import java.util.List;
-
-class Solution {
-    public boolean isPalindrome(ListNode head) {
-        // 1. init
-        ListNode rev = null;
-        ListNode fast = head;
-        ListNode slow = head;
-
-        // 2. runner
-        while(fast != null && fast.next != null) {
-            fast = fast.next.next;
-
-            ListNode temp = slow;
-            ListNode prev = rev;
-            slow = slow.next;
-            rev = temp;
-            rev.next = prev;
+class Main {
+    public static void main(String[] args) {
+        StringBuffer a = new StringBuffer("a");
+        StringBuffer b = a;
+        for(int i=0; i<1000; i++) {
+            a.append("c");
         }
 
-        // 3. check
-        if(fast != null) slow = slow.next;
-
-        // 4. loop
-        while(rev != null && rev.val == slow.val) {
-            slow = slow.next;
-            rev = rev.next;
-        }
-
-        return rev == null;
+        System.out.println(a);
+        System.out.println(b);
     }
-}
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
 }
