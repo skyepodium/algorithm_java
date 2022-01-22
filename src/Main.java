2699c099f89a3650e5a84a5cefcdaaf3fcf00ddd
skyepodium <skyepodium@gmail.com>
2022-01-22 13:24:31 +0900

    reorder-data-in-log-files

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 class Solution {
-    public boolean isPalindrome(String s) {
-        // 1. replace
-        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+    public String[] reorderLogFiles(String[] logs) {
 
-        // 2. two pointer - loop
-        int l = 0;
-        int r = s.length() - 1;
-        while(l < r) {
-            if(s.charAt(l) != s.charAt(r)) return false;
-            l++;
-            r--;
-        }
+        // 1. init
+        List<String> letters = new ArrayList<>();
+        List<String> digits = new ArrayList<>();
 
-        return true;
+        // 2. loop
+        Arrays.stream(logs).forEach(x -> {
+            if(isDigit(x.split(" ")[1])) digits.add(x);
+            else letters.add(x);
+        });
+
+        // 3. sort
+        Collections.sort(letters, (a, b) -> {
+            String[] first = a.split(" ", 2);
+            String[] second = b.split(" ", 2);
+
+            if(first[1].equals(second[1])) {
+                return first[0].compareTo(second[0]);
+            } else{
+                return first[1].compareTo(second[1]);
+            }
+        });
+
+        // 4. to array
+        letters.addAll(digits);
+        String[] res = new String[letters.size()];
+        return letters.toArray(res);
     }
-}
 
-class Main {
-    public static void main(String[] args) {
-
+    // 5. check isDigit
+    public Boolean isDigit(String s) {
+        int size = s.length();
+        for(int i=0; i<size; i++) {
+            if(!Character.isDigit(s.charAt(i))) return false;
+        }
+        return true;
     }
 }
