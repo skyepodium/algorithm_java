b36f1953bfbbbd6c4e2ac44d344610abbd1689f8
skyepodium <skyepodium@gmail.com>
2022-01-22 23:24:42 +0900

    two-sum with O(n)

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,26 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         // 1. init
         int[] res = new int[2];
-        List<Info> numList = new ArrayList<>();
+        Map<Integer, Integer> m = new HashMap<>();
+
+        // 2. loop
         for(int i=0; i<nums.length; i++) {
-            numList.add(new Info(nums[i], i));
-        }
-
-        // 2. sort
-        numList.sort((a, b) -> {
-            return a.num - b.num;
-        });
-
-        // 3. two pointer
-        int l = 0;
-        int r = nums.length - 1;
-        while(l < r) {
-            int cur = numList.get(l).num + numList.get(r).num;
-
-            if(cur < target) {
-                l++;
-            }
-            else if(cur > target) {
-                r--;
+            int cur = nums[i];
+            int remain = target - cur;
+            if(m.containsKey(remain)) {
+                int idx = m.get(remain);
+                res[0] = idx;
+                res[1] = i;
+                break;
             }
             else {
-                res[0] = numList.get(l).idx;
-                res[1] = numList.get(r).idx;
-                break;
+                m.put(cur, i);
             }
         }
 
         return res;
-    }
-}
-
-class Info {
-    int num;
-    int idx;
-
-    public Info(int num, int idx) {
-        this.num = num;
-        this.idx = idx;
     }
 }
