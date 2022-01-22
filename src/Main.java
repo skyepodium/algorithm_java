76250f3806e7e0ae5428e3a148fad870ef5fe045
skyepodium <skyepodium@gmail.com>
2022-01-22 12:32:10 +0900

    valid-palindrome - 1. regex, 2. two pointer

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,23 @@
-class Main {
-    public static void main(String[] args) {
-        StringBuffer a = new StringBuffer("a");
-        StringBuffer b = a;
-        for(int i=0; i<1000; i++) {
-            a.append("c");
+class Solution {
+    public boolean isPalindrome(String s) {
+        // 1. replace
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        // 2. two pointer - loop
+        int l = 0;
+        int r = s.length() - 1;
+        while(l < r) {
+            if(s.charAt(l) != s.charAt(r)) return false;
+            l++;
+            r--;
         }
 
-        System.out.println(a);
-        System.out.println(b);
+        return true;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+
     }
 }
