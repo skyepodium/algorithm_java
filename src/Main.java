eac4d53887b3bfec0a2f454fe876600487d8105d
skyepodium <skyepodium@gmail.com>
2022-01-24 02:01:05 +0900

    enum singleton - thread safe

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ class Main {
         // 2. 반복문을 통해 - 10개의 스레드가 동시에 인스턴스 생성
         for (int i = 0; i < 10; i++) {
             service.submit(() -> {
-                SingleTon.getInstance();
+                System.out.println(SingleTon.INSTANCE);
             });
         }
         // 3. 종료
@@ -17,20 +17,6 @@ class Main {
     }
 }
 
-// 싱글톤 클래스
-class SingleTon {
-    // private 생성자 new 를 통한 인스턴스 생성 방지
-    private SingleTon() {
-        System.out.println("싱글톤 인스턴스 생성");
-    }
-
-    // 정적 메서드를 통해 내부 클래스 로딩
-    public static SingleTon getInstance() {
-        return LazyHolder.INSTANCE;
-    }
-
-    // 내부 클래스가 로딩될때 초기화 수행 - 싱글톤 인스턴스 생성
-    private static class LazyHolder {
-        private static final SingleTon INSTANCE = new SingleTon();
-    }
+enum SingleTon {
+    INSTANCE;
 }
