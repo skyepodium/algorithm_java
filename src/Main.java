66289d87e302e2e45c5ae4e431bab2c45a708098
skyepodium <skyepodium@gmail.com>
2022-01-28 23:34:46 +0900

    defanging-an-ip-address

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,5 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-
 class Solution {
-    public int[] sortByBits(int[] arr) {
-        return Arrays.stream(arr)
-                .boxed()
-                .sorted((a, b) -> {
-                    int aOneCnt = Integer.bitCount(a);
-                    int bOneCnt = Integer.bitCount(b);
-                    return aOneCnt == bOneCnt ? a - b : aOneCnt - bOneCnt;
-                })
-                .mapToInt(i -> i)
-                .toArray();
+    public String defangIPaddr(String address) {
+        return address.replaceAll("\\.", "[.]");
     }
-}
-
-class Main {
-
 }
