9ecf79d704e6dd9c87b8aabbce16f369c2745d43
skyepodium <skyepodium@gmail.com>
2022-01-31 00:03:14 +0900

    leet code capitalize-the-title

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
-class Solution {
-    public boolean detectCapitalUse(String word) {
+import java.util.Locale;
 
-        return word.equals(word.toUpperCase()) || word.equals(word.toLowerCase()) || word.equals(word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase());
+class Solution {
+    public String capitalizeTitle(String title) {
+        // 1. init
+        String[] titleArr = title.split(" ");
+        StringBuilder res = new StringBuilder();
+
+        // 2. loop
+        for(int i=0; i<titleArr.length; i++) {
+            String t = titleArr[i];
+            res.append(t.length() > 2 ? t.substring(0, 1).toUpperCase() + t.substring(1).toLowerCase() : t.toLowerCase());
+            if(i != titleArr.length - 1) res.append(" ");
+        }
+
+        return res.toString();
     }
 }
