516433c46f2d21966a4f200e10a5e633ed9750c0
skyepodium <skyepodium@gmail.com>
2022-01-31 01:10:48 +0900

    find-common-characters

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    public int findSpecialInteger(int[] arr) {
+    public List<String> commonChars(String[] words) {
         // 1. init
-        int n = arr.length;
-        int q = n / 4;
+        int n = words.length;
+        String origin = words[0];
+        List<String> res = new ArrayList<>();
 
         // 2. loop
-        for(int i=0; i<n - q; i++) {
-            if(arr[i] == arr[i+q]) return arr[i];
+        for(int i=0; i<origin.length(); i++) {
+            char c = origin.charAt(i);
+            int cnt = 0;
+            for(int j=0; j<n; j++) {
+                String cur = words[j];
+                for(int k=0; k<cur.length(); k++) {
+                    if(cur.charAt(k) == c) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            if(cnt == n) {
+                res.add(c + "");
+                for(int q=0; q<n; q++) {
+                    words[q] = words[q].replaceFirst(c+"", "");
+                }
+            }
         }
 
-        return -1;
+        return res;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+
     }
 }
