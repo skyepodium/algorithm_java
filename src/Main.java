03f2c3c229e2101e9aa425d98ee161c958047e7e
skyepodium <skyepodium@gmail.com>
2022-01-30 23:40:53 +0900

    leet code maximum-number-of-words-you-can-type

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,31 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
-    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+    public int canBeTypedWords(String text, String brokenLetters) {
         // 1. init
-        Map<Integer, Integer> m = new HashMap<>();
-        List<Integer> aList = new ArrayList<>();
-        List<Integer> bList = new ArrayList<>();
-        int[] res = new int[arr1.length];
+        String[] textList = text.split(" ");
+        int res = 0;
+        Set<Character> c = new HashSet<>();
+
+        for(int i=0; i<brokenLetters.length(); i++) {
+            c.add(brokenLetters.charAt(i));
+        }
+
 
         // 2. loop
-        for(int i=0; i<arr2.length; i++) {
-            m.put(arr2[i], i);
-        }
-
-        // 3. search
-        for(int i=0; i<arr1.length; i++) {
-            int cur = arr1[i];
-
-            if(m.containsKey(cur)) {
-                aList.add(cur);
-            }else{
-                bList.add(cur);
+        for(String w: textList) {
+            boolean isOk = true;
+            for(int i=0; i<w.length(); i++) {
+                if(c.contains(w.charAt(i))) {
+                    isOk = false;
+                    break;
+                }
             }
+            if(isOk) res++;
         }
 
-        aList.sort(Comparator.comparingInt(m::get));
-        bList.sort(Comparator.comparingInt(a -> a));
 
-        aList.addAll(bList);
-
-        return aList.stream().mapToInt(Integer::intValue).toArray();
+        return res;
     }
 }
