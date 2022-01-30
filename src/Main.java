9ce72d21616fbe2bfdca33f2ebcedee738d69a44
skyepodium <skyepodium@gmail.com>
2022-01-30 23:53:33 +0900

    leet code https://leetcode.com/problems/detect-capital/submissions/

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,6 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
-    public int canBeTypedWords(String text, String brokenLetters) {
-        // 1. init
-        String[] textList = text.split(" ");
-        int res = 0;
-        Set<Character> c = new HashSet<>();
+    public boolean detectCapitalUse(String word) {
 
-        for(int i=0; i<brokenLetters.length(); i++) {
-            c.add(brokenLetters.charAt(i));
-        }
-
-
-        // 2. loop
-        for(String w: textList) {
-            boolean isOk = true;
-            for(int i=0; i<w.length(); i++) {
-                if(c.contains(w.charAt(i))) {
-                    isOk = false;
-                    break;
-                }
-            }
-            if(isOk) res++;
-        }
-
-
-        return res;
+        return word.equals(word.toUpperCase()) || word.equals(word.toLowerCase()) || word.equals(word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase());
     }
 }
