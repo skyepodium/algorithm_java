84331ddc59ac2d227548fd3dc1afbdb7b5e2cfb0
skyepodium <skyepodium@gmail.com>
2022-02-01 00:26:32 +0900

    richest-customer-wealth with stream

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        // 1. init
-        AtomicInteger result = new AtomicInteger();
-
-        Arrays.stream(accounts).forEach(x -> {
-            result.set(Math.max(result.get(), getSum(x)));
-        });
-
-        return result.get();
-    }
-
-    public int getSum(int[] a) {
-        int res = 0;
-        for(int i=0; i<a.length; i++) {
-            res += a[i];
-        }
-        return res;
+        return Arrays.stream(accounts).mapToInt(x -> Arrays.stream(x).sum()).max().getAsInt();
     }
 }
