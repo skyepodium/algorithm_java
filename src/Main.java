591a85fc2fe31952eff34bc3c7670a2c95b76807
skyepodium <skyepodium@gmail.com>
2022-02-01 01:26:35 +0900

    top-k-frequent-words

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,34 @@
 import java.util.*;
 
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
+    public List<String> topKFrequent(String[] words, int k) {
         // 1. init
-        int[] res = new int[k];
-        Map<Integer, Integer> m = new HashMap<>();
-        List<Info> numList = new ArrayList<>();
+        List<String> res = new ArrayList<>();
+        Map<String, Integer> m = new HashMap<>();
+        List<Info> wList = new ArrayList<>();
         int idx = 0;
 
-        // 2. count
-        Arrays.stream(nums).forEach(x -> {
+        // 2. counter
+        Arrays.stream(words).forEach(x -> {
             if(m.containsKey(x)) m.put(x, m.get(x) + 1);
             else m.put(x, 1);
         });
 
-        // 3. to list
-        m.keySet().stream().forEach(x -> {
-            numList.add(new Info(x, m.get(x)));
+        // 3. map to list
+        m.keySet().forEach(x -> {
+            wList.add(new Info(x, m.get(x)));
         });
 
         // 4. sort
-        numList.sort((a, b) -> b.cnt - a.cnt);
+        wList.sort((a, b) -> {
+            if(a.cnt == b.cnt) return a.w.compareTo(b.w);
+            else return b.cnt - a.cnt;
+        });
 
-        // 5. result
-        for(Info info: numList) {
+        // 5. loop
+        for(Info info: wList) {
             if(idx >= k) break;
-            res[idx] = info.num;
+            res.add(info.w);
             idx++;
         }
 
@@ -34,23 +37,10 @@ class Solution {
 }
 
 class Info {
-    int num;
+    String w;
     int cnt;
-    public Info (int num, int cnt) {
-        this.num = num;
+    public Info(String w, int cnt) {
+        this.w = w;
         this.cnt = cnt;
-    }
-}
-
-class Main {
-    public static void main(String[] args) {
-
-        Solution sl = new Solution();
-        int[] nums = {1,1,1,2,2,3};
-        int[] res = sl.topKFrequent(nums, 2);
-        for(int i=0; i<res.length; i++) {
-            System.out.print(res[i] + " ");
-        }
-        System.out.println();
     }
 }
