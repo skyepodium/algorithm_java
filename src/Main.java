e25c9c5d5e0a9762062b8b9740ee1de066be2486
skyepodium <skyepodium@gmail.com>
2022-02-01 01:10:29 +0900

    top-k-frequent-elements

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,56 @@
-import java.util.Arrays;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.*;
 
 class Solution {
-    public int maximumWealth(int[][] accounts) {
-        return Arrays.stream(accounts).mapToInt(x -> Arrays.stream(x).sum()).max().getAsInt();
+    public int[] topKFrequent(int[] nums, int k) {
+        // 1. init
+        int[] res = new int[k];
+        Map<Integer, Integer> m = new HashMap<>();
+        List<Info> numList = new ArrayList<>();
+        int idx = 0;
+
+        // 2. count
+        Arrays.stream(nums).forEach(x -> {
+            if(m.containsKey(x)) m.put(x, m.get(x) + 1);
+            else m.put(x, 1);
+        });
+
+        // 3. to list
+        m.keySet().stream().forEach(x -> {
+            numList.add(new Info(x, m.get(x)));
+        });
+
+        // 4. sort
+        numList.sort((a, b) -> b.cnt - a.cnt);
+
+        // 5. result
+        for(Info info: numList) {
+            if(idx >= k) break;
+            res[idx] = info.num;
+            idx++;
+        }
+
+        return res;
+    }
+}
+
+class Info {
+    int num;
+    int cnt;
+    public Info (int num, int cnt) {
+        this.num = num;
+        this.cnt = cnt;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+
+        Solution sl = new Solution();
+        int[] nums = {1,1,1,2,2,3};
+        int[] res = sl.topKFrequent(nums, 2);
+        for(int i=0; i<res.length; i++) {
+            System.out.print(res[i] + " ");
+        }
+        System.out.println();
     }
 }
