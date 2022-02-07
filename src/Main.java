38828938ad40a7c0d3449e5f27f847ffa13dc077
skyepodium <skyepodium@gmail.com>
2022-02-08 02:08:07 +0900

    프로그래머스 - 베스트 앨범

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,67 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 class Solution {
-    public int solution(String[][] clothes) {
-        // 1. 초기화
-        int res = 0;
-        Map<String, Integer> m = new HashMap<>();
+    public int[] solution(String[] genres, int[] plays) {
+        // 1. init
+        Map<String, Integer> c = new HashMap<>();
+        Map<String, List<Info>> i = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        int[] res;
 
-        // 2. 카운트
-        for(String[] c: clothes) {
-            m.put(c[1], m.getOrDefault(c[1], 0) + 1);
+        for(int idx=0; idx<genres.length; idx++) {
+            String g = genres[idx];
+            int p = plays[idx];
+
+            c.put(g, c.getOrDefault(g, 0) + p);
+
+            List<Info> tempList;
+            if (i.containsKey(g)) tempList = i.get(g);
+            else tempList = new ArrayList<>();
+            tempList.add(new Info(idx, p));
+            i.put(g, tempList);
         }
 
-        // 3. 인수분해
-        for(Integer x: m.values()) {
-            res *= x + 1;
+        List<Count> l = new ArrayList<>();
+        for(String key: c.keySet()) {
+            l.add(new Count(key, c.get(key)));
         }
 
-        return res - 1;
+        l.sort((a, b) -> b.count - a.count);
+
+        for(Count cur: l) {
+            List<Info> songList = i.get(cur.genre);
+            songList.sort((a, b) -> {
+                if(a.count == b.count) return a.idx - b.idx;
+                else return b.count - a.count;
+            });
+            result.add(songList.get(0).idx);
+            if(songList.size() >= 2) result.add(songList.get(1).idx);
+        }
+
+        res = new int[result.size()];
+        for(int idx=0; idx<res.length; idx++) {
+            res[idx] = result.get(idx);
+        }
+
+        return res;
+    }
+}
+class Count {
+    String genre;
+    int count;
+    public Count(String genre, int count) {
+        this.genre = genre;
+        this.count = count;
+    }
+}
+class Info {
+    int idx;
+    int count;
+    public Info(int idx, int count) {
+        this.idx = idx;
+        this.count = count;
     }
 }
