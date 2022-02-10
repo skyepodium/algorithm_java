d44b0ac9cee5ea23fc13056f4dd05c840bf60aa9
skyepodium <skyepodium@gmail.com>
2022-02-10 23:11:32 +0900

    check-if-n-and-its-double-exist

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,14 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
-    public int[] solution(String[] genres, int[] plays) {
-        // 1. init
-        Map<String, Integer> c = new HashMap<>();
-        Map<String, List<Info>> i = new HashMap<>();
-        List<Integer> result = new ArrayList<>();
-        int[] res;
+    public boolean checkIfExist(int[] arr) {
+        Set<Integer> s = new HashSet<>();
+        for(int a: arr) {
+            if(s.contains(a * 2) || ((a % 2) == 0 && s.contains(a / 2))) return true;
 
-        for(int idx=0; idx<genres.length; idx++) {
-            String g = genres[idx];
-            int p = plays[idx];
-
-            c.put(g, c.getOrDefault(g, 0) + p);
-
-            List<Info> tempList;
-            if (i.containsKey(g)) tempList = i.get(g);
-            else tempList = new ArrayList<>();
-            tempList.add(new Info(idx, p));
-            i.put(g, tempList);
+            s.add(a);
         }
-
-        List<Count> l = new ArrayList<>();
-        for(String key: c.keySet()) {
-            l.add(new Count(key, c.get(key)));
-        }
-
-        l.sort((a, b) -> b.count - a.count);
-
-        for(Count cur: l) {
-            List<Info> songList = i.get(cur.genre);
-            songList.sort((a, b) -> {
-                if(a.count == b.count) return a.idx - b.idx;
-                else return b.count - a.count;
-            });
-            result.add(songList.get(0).idx);
-            if(songList.size() >= 2) result.add(songList.get(1).idx);
-        }
-
-        res = new int[result.size()];
-        for(int idx=0; idx<res.length; idx++) {
-            res[idx] = result.get(idx);
-        }
-
-        return res;
-    }
-}
-class Count {
-    String genre;
-    int count;
-    public Count(String genre, int count) {
-        this.genre = genre;
-        this.count = count;
-    }
-}
-class Info {
-    int idx;
-    int count;
-    public Info(int idx, int count) {
-        this.idx = idx;
-        this.count = count;
+        return false;
     }
 }
