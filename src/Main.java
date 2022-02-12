b672c09b07461b88c2e3cbc2152fab5f8461e1e1
skyepodium <skyepodium@gmail.com>
2022-02-12 13:35:09 +0900

    delete-the-middle-node-of-a-linked-list

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
+import java.util.Stack;
+
 class Solution {
-    public ListNode middleNode(ListNode head) {
+    public ListNode deleteMiddle(ListNode head) {
         // 1. init
         ListNode fast = head, slow = head;
+        Stack<Integer> s = new Stack<>();
 
         // 2. runner
         while(fast != null && fast.next != null) {
+            s.add(slow.val);
             slow = slow.next;
             fast = fast.next.next;
         }
 
-        return slow;
+        // 3. stack
+        ListNode res = slow.next;
+        while(!s.empty()) {
+            res = new ListNode(s.pop(), res);
+        }
+
+        return res;
     }
 }
