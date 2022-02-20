5a7cb3fd2520a1c3ad139aad1b0bf1e9061e2076
skyepodium <skyepodium@gmail.com>
2022-02-20 11:33:26 +0900

    reformat-date

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,31 @@
-class Solution {
-    public int[] getNoZeroIntegers(int n) {
-        // 1. loop
-        for(int i=1; i<=n/2 + 1; i++) {
-            if(!nonZero(i) || !nonZero(n-i)) continue;
+import java.util.HashMap;
+import java.util.Map;
 
-            return new int[]{i, n-i};
-        }
-        return new int[]{-1, -1};
+class Solution {
+    public String reformatDate(String date) {
+        // 1. init
+        Map<String, String> m = new HashMap<>();
+        m.put("Jan", "01");
+        m.put("Feb", "02");
+        m.put("Mar", "03");
+        m.put("Apr", "04");
+        m.put("May", "05");
+        m.put("Jun", "06");
+        m.put("Jul", "07");
+        m.put("Aug", "08");
+        m.put("Sep", "09");
+        m.put("Oct", "10");
+        m.put("Nov", "11");
+        m.put("Dec", "12");
+
+        // 2. split
+        String[] d = date.split(" ");
+        String day = d[0].replaceAll("[a-zA-z]", "");
+
+        return d[2] + "-" + m.get(d[1]) + "-" + zFill(day);
     }
 
-    // 2. nonZero
-    public boolean nonZero(int num) {
-        while(num > 0) {
-            if(num % 10 == 0) return false;
-            num /= 10;
-        }
-        return true;
+    public String zFill(String strNum){
+        return strNum.length() < 2 ? "0" + strNum : strNum;
     }
 }
