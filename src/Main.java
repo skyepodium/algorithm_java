7fc927669c52f11c12b79b990b28a77c55f7ebec
skyepodium <skyepodium@gmail.com>
2022-02-23 21:08:42 +0900

    three-consecutive-odds

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,13 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
-        // 0. exception
-        if(arr.length < 3) return false;
-
-        // 1. init
-        int a = 0;
-        int b = a+1;
-        int c = b+1;
-
-        // 2. loop
-        for(int i=0; i<=arr.length - 3; i++) {
-            if((arr[i+a] & 1) == 1 && (arr[i+b] & 1) == 1 && (arr[i+c] & 1) == 1) return true;
+        int cnt = 0;
+        for(int i=0; i<arr.length; i++) {
+            if((arr[i] & 1) == 0) cnt = 0;
+            else {
+                cnt++;
+                if(cnt >+ 3) return true;
+            }
         }
-
         return false;
     }
 }
