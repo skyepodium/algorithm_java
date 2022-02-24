9dcf1c348af985e0dc5f9a03435b33686de33ce9
skyepodium <skyepodium@gmail.com>
2022-02-25 00:44:29 +0900

    reverse-substrings-between-each-pair-of-parentheses

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,42 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
 class Solution {
-    public boolean threeConsecutiveOdds(int[] arr) {
-        int cnt = 0;
-        for(int i=0; i<arr.length; i++) {
-            if((arr[i] & 1) == 0) cnt = 0;
-            else {
-                cnt++;
-                if(cnt >+ 3) return true;
+    public String reverseParentheses(String s) {
+        // 1. init
+        Stack<String> res = new Stack<>();
+        res.add("");
+
+        // 2. loop
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+
+            if(c == '(') {
+                res.add("");
             }
+            else if(c == ')') {
+                    String t = res.pop();
+                    String y = res.pop();
+                    res.add(y + new StringBuilder(t).reverse());
+            }
+            else {
+                String t = res.pop();
+                res.add(t + c);
+            }
+            System.out.println("res " + res);
         }
-        return false;
+
+        return String.join("", res);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solution sl = new Solution();
+
+        String res = sl.reverseParentheses("(abcd)");
+
+        System.out.println("res " + res);
+
     }
 }
