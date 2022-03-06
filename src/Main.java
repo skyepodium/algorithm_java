08948a697fad1efc55788825c3f65bd8aa4c3f03
skyepodium <skyepodium@gmail.com>
2022-03-06 23:34:00 +0900

    mean-of-array-after-removing-some-elements

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,31 @@
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) {
+        Solution sl = new Solution();
+//        int[] arr = {1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3};
+//        int[] arr = {6,2,7,5,1,2,0,3,10,2,5,0,5,5,0,8,7,6,8,0};
+        int[] arr = {6,0,7,0,7,5,7,8,3,4,0,7,8,1,6,8,1,1,2,4,8,1,9,5,4,3,8,5,10,8,6,6,1,0,6,10,8,2,3,4};
+        double res = sl.trimMean(arr);
+
+        System.out.println("res " + res);
+
+    }
+}
+
 class Solution {
-    public int[] replaceElements(int[] arr) {
-        // 1. init
+    public double trimMean(int[] arr) {
+        // 1. sort
+        Arrays.sort(arr);
+
+        // 2. sub
         int n = arr.length;
-        int[] res = new int[n];
-        res[n-1] = -1;
-        int curMax = arr[n-1];
+        int cnt = (int) (n * 0.05);
+        int m = n - cnt * 2;
+        double s = 0;
 
-        // 2. loop
-        for(int i=n-2; i>=0; i--) {
-            if(arr[i+1] > curMax) curMax = arr[i+1];
-            res[i] = curMax;
-        }
+        for(int i=cnt; i<n-cnt; i++) s += arr[i];
 
-        return res;
+        return s / m;
     }
 }
