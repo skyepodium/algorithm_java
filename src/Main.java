787082b6a4d05ec8c48a377a3c81e7c9f2660a02
Kim Jung yoon <skyepodium@gmail.com>
2022-03-15 12:47:26 +0900

    leetcode - minimum-remove-to-make-valid-parentheses
    leetcode - minimum-remove-to-make-valid-parentheses

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,38 @@
 class Solution {
-    public void duplicateZeros(int[] arr) {
+    public String minRemoveToMakeValid(String s) {
         // 1. init
-        int[] origin = arr.clone();
-        int n = arr.length;
-        int arrIdx = 0;
+        StringBuilder res = new StringBuilder();
+        int cnt = 0;
 
         // 2. loop
-        for(int cur: origin) {
-            if(arrIdx >= n) break;
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
 
-            arr[arrIdx] = cur;
-            arrIdx += 1;
-
-            if(cur == 0 && arrIdx < n) {
-                arr[arrIdx] = cur;
-                arrIdx += 1;
+            if(c == '(') {
+                cnt += 1;
+                res.append(c);
+            }
+            else if(c == ')') {
+                if(cnt > 0) {
+                    cnt -= 1;
+                    res.append(c);
+                }
+            }
+            else {
+                res.append(c);
             }
         }
-     }
+
+        // 3. exception
+        String origin = res.toString();
+        res = new StringBuilder();
+        for(int i=origin.length() - 1; i>=0; i--) {
+            char c = origin.charAt(i);
+
+            if(c == '(' && cnt > 0) cnt -= 1;
+            else res.append(c);
+        }
+
+        return res.reverse().toString();
+    }
 }
