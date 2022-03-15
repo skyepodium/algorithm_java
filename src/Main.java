6b1d550759766ee74d9763102886910475068ef4
skyepodium <skyepodium@gmail.com>
2022-03-15 22:13:27 +0900

    Temp

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,5 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        // 1. init
-        int[] origin = arr.clone();
-        int n = arr.length;
-        int arrIdx = 0;
-
-        // 2. loop
-        for(int cur: origin) {
-            if(arrIdx >= n) break;
-
-            arr[arrIdx] = cur;
-            arrIdx += 1;
-
-            if(cur == 0 && arrIdx < n) {
-                arr[arrIdx] = cur;
-                arrIdx += 1;
-            }
-        }
-     }
+        
+    }
 }
