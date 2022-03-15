cf175f82e2aa7b76e529ca2ef036e1421c1eb570
Kim Jung yoon <skyepodium@gmail.com>
2022-03-15 17:23:04 +0900

    leetcode - min-cost-climbing-stairs
    leetcode - min-cost-climbing-stairs

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,25 @@
 class Solution {
-    public String minRemoveToMakeValid(String s) {
+    public int minCostClimbingStairs(int[] cost) {
         // 1. init
-        StringBuilder res = new StringBuilder();
-        int cnt = 0;
+        int n = cost.length;
+        int[] d = new int[n+1];
+        for(int i=0; i<=n; i++) d[i] = Integer.MAX_VALUE;
 
-        // 2. loop
-        for(int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
+        // 2. dp - bottom up
+        d[0] = cost[0];
+        d[1] = cost[1];
+        for(int i=0; i<n; i++) {
+            if(i+1 <= n) {
+                int c = i + 1 >= n ? 0 : cost[i+1];
+                d[i+1] = Math.min(d[i+1], d[i] + c);
+            }
 
-            if(c == '(') {
-                cnt += 1;
-                res.append(c);
-            }
-            else if(c == ')') {
-                if(cnt > 0) {
-                    cnt -= 1;
-                    res.append(c);
-                }
-            }
-            else {
-                res.append(c);
+            if(i+2 <= n) {
+                int c = i + 2 >= n ? 0 : cost[i+2];
+                d[i+2] = Math.min(d[i+2], d[i] + c);
             }
         }
 
-        // 3. exception
-        String origin = res.toString();
-        res = new StringBuilder();
-        for(int i=origin.length() - 1; i>=0; i--) {
-            char c = origin.charAt(i);
-
-            if(c == '(' && cnt > 0) cnt -= 1;
-            else res.append(c);
-        }
-
-        return res.reverse().toString();
+        return d[n];
     }
 }
