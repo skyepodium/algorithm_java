cba0caceff1410e1aee41c503b1063715b5b1130
skyepodium <skyepodium@gmail.com>
2022-03-22 22:32:30 +0900

    leetcode - check-if-all-as-appears-before-all-bs

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,5 @@
 class Solution {
-
-    public static int res = 0;
-    public static int n = 0;
-    public static int[] b;
-    public static int t = 0;
-    public int findTargetSumWays(int[] nums, int target) {
-        // 1. init
-        n = nums.length;
-        b = nums;
-        t = target;
-        res = 0;
-
-        // 2. dfs
-        go(0, 0);
-
-        return res;
-    }
-
-    public static void go(int idx, int val) {
-        if(idx >= n) {
-            if(val == t) res++;
-            return;
-        }
-
-        go(idx + 1, val + b[idx]);
-        go(idx + 1, val - b[idx]);
+    public boolean checkString(String s) {
+        return !s.contains("ba");
     }
 }
