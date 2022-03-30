9cff917397df0445a1ce0b3e2dbfa474e63ee6f8
skyepodium <skyepodium@gmail.com>
2022-03-30 22:10:10 +0900

    Resolve conflict

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-import java.util.stream.IntStream;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
-    public int[] countBits(int n) {
-        return IntStream
-                .range(0, n+1)
-                .map(x -> {
-                    int count = 0;
-                    while(x > 0) {
-                        if(x%2 == 1) count++;
-                        x /= 2;
-                    }
-                    return count;
-                })
-                .toArray();
+    public int findDuplicate(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+
+        for(int num: nums) {
+            if(s.contains(num)) return num;
+
+            s.add(num);
+        }
+        return -1;
     }
 }
