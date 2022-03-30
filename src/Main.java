c8b42a6b2d6ed786a43b89d0556a599a83165ab6
skyepodium <skyepodium@gmail.com>
2022-03-30 22:30:48 +0900

    leetcode - missing-number

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
-    public int findDuplicate(int[] nums) {
+    public int missingNumber(int[] nums) {
         Set<Integer> s = new HashSet<>();
+        Arrays.stream(nums).forEach(s::add);
 
-        for(int num: nums) {
-            if(s.contains(num)) return num;
-
-            s.add(num);
+        for(int i=0; i<=nums.length; i++) {
+            if(!s.contains(i)) return i;
         }
         return -1;
     }
