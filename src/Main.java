7f46288d09d9fb8aeb2ecb21373861ba466956df
skyepodium <skyepodium@gmail.com>
2022-04-02 11:22:59 +0900

    array list

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,65 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+class Main {
+    public static void main(String[] args) {
+        CustomArrayList<Integer> a = new CustomArrayList<>();
 
-class Solution {
-    public int missingNumber(int[] nums) {
-        Set<Integer> s = new HashSet<>();
-        Arrays.stream(nums).forEach(s::add);
-
-        for(int i=0; i<=nums.length; i++) {
-            if(!s.contains(i)) return i;
+        for(int i=0; i<30; i++) {
+            a.add(i);
         }
-        return -1;
+
+        while(!a.isEmpty()) {
+            System.out.println(a.get(a.getSize() - 1));
+            a.pop();
+        }
+    }
+}
+
+class CustomArrayList <T> {
+    private T[] arr;
+    private int idx = 0;
+    private final int DEFAULT_SIZE = 10;
+    private int MAX_SIZE = 0;
+
+    private void sizeUp() {
+        this.MAX_SIZE = this.MAX_SIZE + (this.MAX_SIZE >> 1);
+    }
+
+    private void copyArr() {
+        T[] temp = (T[]) new Object[this.MAX_SIZE];
+        for(int i=0; i<=this.idx; i++) {
+            temp[i] = this.arr[i];
+        }
+        this.arr = temp;
+    }
+
+    public CustomArrayList() {
+        this.MAX_SIZE = this.DEFAULT_SIZE;
+        this.arr = (T[]) new Object[this.DEFAULT_SIZE];
+    }
+
+    public void add(T val) {
+        if(this.idx == this.MAX_SIZE - 1) {
+            this.sizeUp();
+            this.copyArr();
+        }
+
+        this.arr[idx++] = val;
+    }
+
+    public void pop() {
+        if(this.idx == 0) return;
+
+        this.arr[idx--] = null;
+    }
+
+    public T get(int i) {
+        return this.arr[i];
+    }
+
+    public boolean isEmpty() {
+        return this.idx == 0;
+    }
+
+    public int getSize() {
+        return this.idx;
     }
 }
