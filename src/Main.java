5ebb90e8556b5b35caf36f3fe7ee079e78e11f36
skyepodium <skyepodium@gmail.com>
2022-04-03 17:49:00 +0900

    leetcode - number-of-segments-in-a-string

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
-import java.util.Arrays;
-
 class Solution {
-    public int arraySign(int[] nums) {
-        int res = 1;
+    public int countSegments(String s) {
+        if("".equals(s.trim())) return 0;
 
-        for(int num: nums) {
-            if(num == 0) res = 0;
-            else if(num < 0) res *= -1;
-        }
-
-        return res;
+        return s.trim().split(" +").length;
     }
 }
