791a7d60215436c3542e0b612792428e5ef4c289
skyepodium <skyepodium@gmail.com>
2022-04-04 19:31:09 +0900

    leetcode - decrypt-string-from-alphabet-to-integer-mapping

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
-class Solution {
-    public String licenseKeyFormatting(String s, int k) {
-        s = new StringBuilder(s.replaceAll("-", "").toUpperCase()).reverse().toString();
-        int n = s.length();
+import java.util.LinkedList;
+import java.util.List;
 
-        StringBuilder t = new StringBuilder();
-        for(int i=0; i<n; i+=k) {
-            t.append(s.substring(0, i+k));
-            if(i+k < n) t.append("-");
+class Main {
+    public static void main(String[] args) {
+
+        List<Integer> a = new LinkedList<>();
+
+        a.remove(3);
+    }
+}
+
+class Solution {
+    public String freqAlphabets(String s) {
+
+        for(int i=26; i>=0; i--) {
+            String pattern = i >= 10 ? i + "#" : i + "";
+            s = s.replaceAll(pattern, getAlphabet(i));
         }
 
-        return t.reverse().toString();
+        return s;
+    }
+
+    public String getAlphabet(int n) {
+        return (char)((int)'a' + n - 1) + "";
     }
 }
