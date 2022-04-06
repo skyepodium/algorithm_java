00b90de670232c2e761c43e8cdf57c0a67eff647
skyepodium <skyepodium@gmail.com>
2022-04-07 01:04:19 +0900

    leetcode - divide-a-string-into-groups-of-size-k

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,46 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class Main {
     public static void main(String[] args) {
-
         Solution sl = new Solution();
-        int[] nums = {1, 1, 1, 1};
-        int res = sl.sumOfUnique(nums);
 
-        System.out.println(res);
+        String s = "abcdefghi";
+        int k = 3;
+        char fill = 'x';
+
+
+//        String s = "abcdefghij";
+//        int k = 3;
+//        char fill = 'x';
+
+        String[] res = sl.divideString(s, k, fill);
+
+        for(String a: res) {
+            System.out.println(a);
+        }
     }
 }
 
 class Solution {
-    public int sumOfUnique(int[] nums) {
-        Map<Integer, Integer> m = new HashMap<>();
+    public String[] divideString(String s, int k, char fill) {
+        // 1. init
+        List<String> res = new ArrayList<>();
+        int n = s.length();
+        int cnt = n % k != 0 ? n / k + 1 : n / k;
 
-        Arrays.stream(nums).forEach(x -> m.put(x, m.getOrDefault(x, 0) + 1));
+        StringBuilder base = new StringBuilder();
+        for(int i=0; i<k; i++) base.append(fill);
+        String b = base.toString();
 
-        return m.entrySet().stream().filter(x -> x.getValue() == 1)
-                .map(Map.Entry::getKey)
-                .reduce(Integer::sum)
-                .orElse(0);
+        // 2. loop
+        for(int i=0; i<n; i+=k) {
+            res.add(s.substring(i, Math.min(i+k, n)));
+        }
+
+        // 3. map
+        return res.stream().map(x -> x.length() < k ? x + b.substring(0, k - x.length()) : x)
+                .collect(Collectors.toList())
+                .toArray(new String[cnt]);
     }
 }
