58df63b1720480c6976c6650213d0453bd3b6135
skyepodium <skyepodium@gmail.com>
2022-04-09 11:00:45 +0900

    leetcode - kth-largest-element-in-a-stream

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,25 @@
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
-class Main {
-    public static void main(String[] args) {
-        Solution sl = new Solution();
+class KthLargest {
+    private PriorityQueue<Integer> pq;
+    private int k;
 
-        String s = "abcdefghi";
-        int k = 3;
-        char fill = 'x';
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
 
+        this.pq = new PriorityQueue<>(Comparator.comparingInt(a -> a));
+        Arrays.stream(nums).forEach(x -> pq.add(x));
 
-//        String s = "abcdefghij";
-//        int k = 3;
-//        char fill = 'x';
-
-        String[] res = sl.divideString(s, k, fill);
-
-        for(String a: res) {
-            System.out.println(a);
-        }
+        while(pq.size() > k) pq.poll();
     }
-}
 
-class Solution {
-    public String[] divideString(String s, int k, char fill) {
-        // 1. init
-        List<String> res = new ArrayList<>();
-        int n = s.length();
-        int cnt = n % k != 0 ? n / k + 1 : n / k;
+    public int add(int val) {
+        this.pq.add(val);
 
-        StringBuilder base = new StringBuilder();
-        for(int i=0; i<k; i++) base.append(fill);
-        String b = base.toString();
+        while(pq.size() > k) pq.poll();
 
-        // 2. loop
-        for(int i=0; i<n; i+=k) {
-            res.add(s.substring(i, Math.min(i+k, n)));
-        }
-
-        // 3. map
-        return res.stream().map(x -> x.length() < k ? x + b.substring(0, k - x.length()) : x)
-                .collect(Collectors.toList())
-                .toArray(new String[cnt]);
+        return pq.peek();
     }
 }
