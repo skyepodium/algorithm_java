d6a6f515a08ab60aa73d8f9b50ccecbb6ea63013
skyepodium <skyepodium@gmail.com>
2022-04-09 23:47:06 +0900

    정수 제곱근 판별

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,7 @@
-import java.util.Arrays;
-
 class Solution {
-    public int solution(int n) {
-        // 1. init
-        int[] d = new int[n+1];
-        for(int i=0; i<=n; i++) d[i] = 1;
+    public long solution(long n) {
+        long num = (long) Math.sqrt(n);
 
-        // 2. eratos
-        for(int i=2; i*i<=n; i++) {
-            for(int j=i*2; j<=n; j+=i) {
-                d[j] = 0;
-            }
-        }
-        d[0] = d[1] = 0;
-
-        return Arrays.stream(d).sum();
+        return num * num == n ? (long) Math.pow(num + 1, 2) : -1;
     }
 }
