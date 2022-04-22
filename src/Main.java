5efceb75b0b589b16aa991767331322be1f24626
skyepodium <skyepodium@gmail.com>
2022-04-22 17:06:06 +0900

    leetcode - largest-number-at-least-twice-of-others

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,29 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.PriorityQueue;
+import java.util.stream.IntStream;
 
 class Solution {
-    public int findFinalValue(int[] nums, int original) {
-        Set<Integer> s = new HashSet<>(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+    public int dominantIndex(int[] nums) {
+        // 1. exception
+        if(nums.length < 2) return 0;
 
-        while(s.contains(original)) original *= 2;
+        // 2. init
+        PriorityQueue<Info> pq = new PriorityQueue<>((a, b) -> b.val - a.val);
 
-        return original;
+        IntStream.range(0, nums.length).forEach(x -> pq.add(new Info(nums[x], x)));
+
+        Info first = pq.poll();
+        Info second = pq.poll();
+
+        return first.val >= 2 * second.val ? first.idx : -1;
+    }
+}
+
+class Info {
+    public int val;
+    public int idx;
+
+    public Info(int val, int idx) {
+        this.val = val;
+        this.idx = idx;
     }
 }
