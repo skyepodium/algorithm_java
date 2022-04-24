a4bcd4c4d9777dfa9e1eaeda800cad256187f5ef
skyepodium <skyepodium@gmail.com>
2022-04-24 17:12:09 +0900

    [1차] 프렌즈4블록

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,114 @@
-import java.util.*;
-import java.util.stream.Collectors;
-
 class Solution {
-    Map<String, List<String>> d;
-    Map<String, Integer> check;
-    public int solution(String begin, String target, String[] words) {
+    int[] dx = {0, 0, 1, 1};
+    int[] dy = {0, 1, 0, 1};
+    String[][] b;
+    boolean[][] check;
+    public int solution(int m, int n, String[] board) {
         // 1. init
-        d = new HashMap<>();
-        check = new HashMap<>();
-        List<String> w = Arrays.stream(words).collect(Collectors.toList());
-        w.add(begin);
-
-        // 2. loop
-        for(int i=0; i<w.size(); i++) {
-            for(int j=0; j<w.size(); j++) {
-                if(i == j) continue;
-                String a = w.get(i);
-                String b = w.get(j);
-                if(getDiff(a, b) == 1) {
-                    List<String> q = d.getOrDefault(a, new ArrayList<>());
-                    q.add(b);
-                    d.put(a, q);
-
-                    List<String> e = d.getOrDefault(b, new ArrayList<>());
-                    e.add(a);
-                    d.put(b, e);
-                }
+        int res = 0;
+        int temp = n;
+        n = m;
+        m = temp;
+        b = new String[n][m];
+        check = new boolean[n][m];
+        for(int i=0; i<n; i++) {
+            String[] t = board[i].split("");
+            for(int j=0; j<m; j++) {
+                b[i][j] = t[j];
             }
         }
 
-        // 3. bfs
-        bfs(begin);
+        while(true) {
+            // 1) init
+            init(n, m);
 
-        return check.getOrDefault(target, 0);
-    }
+            // 2) search
+            int searchCnt = search(n, m);
+            if(searchCnt == 0) break;
+            res += searchCnt;
 
-    public int getDiff(String a, String b) {
-        int res = 0;
-        int n = Math.min(a.length(), b.length());
-        for(int i=0; i<n; i++) {
-            if(a.charAt(i) != b.charAt(i)) res++;
+            // 3) clear
+            clear(n, m);
+
+            // 4) move
+            move(n, m);
         }
+
         return res;
     }
 
-    public void bfs(String startNode) {
-        Queue<String> q = new LinkedList<>();
-        q.add(startNode);
-        check.put(startNode, 0);
+    public void init(int n, int m) {
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++)
+                check[i][j] = false;
+    }
 
-        while(!q.isEmpty()) {
-            String node = q.poll();
-            for(String nNode: d.getOrDefault(node, new ArrayList<>())) {
-                if(!check.containsKey(nNode)) {
-                    check.put(nNode, check.get(node) + 1);
-                    q.add(nNode);
+    public int search(int n, int m) {
+        int cnt = 0;
+
+        for(int i=0; i<n-1; i++) {
+            for(int j=0; j<m-1; j++) {
+                String prev = b[i][j];
+                if(prev.equals("")) continue;
+
+                boolean isSame = true;
+                for(int k=0; k<4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    String cur = b[nx][ny];
+                    if(!prev.equals(cur)) {
+                        isSame = false;
+                        break;
+                    }
+                }
+
+                if(!isSame) continue;
+
+                for(int k=0; k<4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if(check[nx][ny]) continue;
+                    check[nx][ny] = true;
+                    cnt++;
                 }
             }
         }
+
+        return cnt;
+    }
+
+    public void move(int n, int m) {
+        for(int j=0; j<m; j++) {
+            for(int idx=n-1; idx>=0; idx--) {
+                int i = idx;
+                if(b[i][j].equals("") || check[i][j]) continue;
+
+                while(i < n-1 && !b[i][j].equals("") && b[i+1][j].equals("")) {
+                    b[i+1][j] = b[i][j];
+                    b[i][j] = "";
+                    i++;
+                }
+            }
+        }
+    }
+
+    public void clear(int n, int m) {
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++)
+                if(check[i][j]) b[i][j] = "";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int m = 4;
+        int n = 5;
+        String[] board = {"CCBDE", "AAADE", "AAABF", "CCBBF"};
+
+        Solution sl = new Solution();
+
+        int res = sl.solution(m, n, board);
+
+        System.out.println("res " + res);
     }
 }
