054f8f7ef3ff561817040f1c5e4cc4520fd74eab
skyepodium <skyepodium@gmail.com>
2022-04-24 23:39:59 +0900

    프로그래머스 - 숫자의 표현

--- src/Main.java
+++ src/Main.java
@@ -1,95 +1,24 @@
-import java.util.*;
-import java.util.concurrent.atomic.AtomicBoolean;
-
 class Solution {
-    List<List<Integer>> lList;
-    int m;
-    public int solution(String[][] relation) {
+    public int solution(int n) {
         // 1. init
         int res = 0;
-        int n = relation.length;
-        m = relation[0].length;
-        Set<String> idxSet = new HashSet<>();
-        lList = new ArrayList<>();
+        int[] d = new int[n+1];
+        for(int i=1; i<=n; i++) d[i] = d[i-1] + i;
 
-        // 2. go
-        go(0, new Stack<>());
-
-        // 3. sort
-        lList.sort(Comparator.comparingInt(List::size));
-
-        // 4. loop
-        for(List<Integer> l: lList) {
-            // 1) uniqueness
-            Set<String> cSet = new HashSet<>();
-            l.forEach(x -> cSet.add(Integer.toString(x)));
-
-            AtomicBoolean isPossible = new AtomicBoolean(true);
-
-            idxSet.stream().forEach(idx -> {
-                int cnt = 0;
-
-                for(String x: idx.split("")) {
-                    if(cSet.contains(x)) cnt++;
-                }
-
-                if(cnt == idx.length()) {
-                    isPossible.set(false);
-                }
-            });
-
-            if(!isPossible.get()) continue;
-
-            // 2)  make key
-            List<String> keyList = new ArrayList<>();
-            for(String[] c: relation) {
-                String key = "";
-                for(int x: l) {
-                    key += c[x] + "_";
-                }
-                keyList.add(key);
+        // 2. two pointer
+        int s = 0;
+        int e = 0;
+        while(s <= e && e < n + 1) {
+            int cur = d[e] - d[s];
+            if(cur <= n) {
+                if(cur == n) res++;
+                e++;
             }
-
-            // 3) size check
-            Set<String> s = new HashSet<>(keyList);
-            if(s.size() < n) continue;
-
-            // 4) add key
-            String nKey = "";
-            for(int i: l) {
-                nKey += Integer.toString(i);
+            else {
+                s++;
             }
-            idxSet.add(nKey);
-            res++;
         }
 
         return res;
-    }
-
-    public void go(int idx, Stack<Integer> l) {
-        if(idx >= m) {
-            if(l.size() > 0) {
-                lList.add(new ArrayList<>(l));
-            }
-            return;
-        }
-
-        l.add(idx);
-        go(idx + 1, l);
-        l.pop();
-
-        go(idx + 1, l);
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        String[][] relation = {{"100","ryan","music","2"}};
-
-        Solution sl = new Solution();
-
-        int res = sl.solution(relation);
-
-        System.out.println("res " + res);
     }
 }
