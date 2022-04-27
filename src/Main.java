00c7a36707c904e181fa729b7940cc2f53f1c2f6
Kim Jung yoon <skyepodium@gmail.com>
2022-04-27 11:22:29 +0900

    프로그래머스 - 입국심사
    프로그래머스 - 입국심사

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,31 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.Arrays;
 
 class Solution {
-    int[] d;
-    public int minCostConnectPoints(int[][] points) {
+    public long solution(int n, int[] times) {
         // 1. init
-        int n = points.length;
-        d = new int[n];
-        for(int i=0; i<n; i++) d[i] = i;
-        List<Point> l = new ArrayList<>();
-        int res = 0;
+        long maxVal = Arrays.stream(times).max().getAsInt();
+        long res = maxVal * n;
+        long s = 0, e = maxVal * n;
 
-        // 2. make graph
-        for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                int dist = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
-                l.add(new Point(i, j, dist));
+        // 2. binary search
+        while(s < e) {
+            long mid = s + (e - s) / 2;
+            long cnt = calTime(mid, times);
+
+            if(cnt < n) {
+                s = mid + 1;
             }
-        }
-
-        // 3. sort
-        l.sort(Comparator.comparingInt(a -> a.cost));
-
-        // 4. cost
-        for(Point p: l) {
-            int s = find(p.start);
-            int e = find(p.end);
-            int c = p.cost;
-
-            if(s != e) {
-                d[s] = e;
-                res += c;
+            else {
+                res = Math.min(res, mid);
+                e = mid;
             }
         }
 
         return res;
     }
 
-    public int find(int node) {
-        if(node == d[node]) return node;
-        else return d[node] = find(d[node]);
-    }
-}
-
-class Point {
-    int start;
-    int end;
-    int cost;
-    public Point(int start, int end, int cost) {
-        this.start = start;
-        this.end = end;
-        this.cost = cost;
+    public long calTime(long a, int[] times) {
+        return Arrays.stream(times).mapToLong(x -> a/x)
+                .reduce(0, Long::sum);
     }
 }
