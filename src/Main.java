049d46bf9999ae01c73ae7c1b75baeb62bbe363f
skyepodium <skyepodium@gmail.com>
2022-04-28 23:43:10 +0900

    leetcode - path-with-minimum-effort

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,61 @@
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
 class Solution {
-    int[][][] d;
-    int[] dx = {0, 0, 1, -1};
-    int[] dy = {-1, 1, 0, 0};
-
-    public int solution(int[][] board) {
+    int n, m;
+    int[][] d;
+    int[][] dir = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
+    public int minimumEffortPath(int[][] heights) {
         // 1. init
-        int n = board.length;
-        int m = board[0].length;
-        int maxVal = n * m * 600;
-        d = new int[n][m][4];
+        int maxVal = 1000000;
+        n = heights.length;
+        m = heights[0].length;
+        d = new int[n][m];
         for(int i=0; i<n; i++)
             for(int j=0; j<m; j++)
-                for(int k=0; k<4; k++)
-                    d[i][j][k] = maxVal;
+                d[i][j] = maxVal;
 
-        dijkstra(0, 0, n, m, board);
+        // 2. dijkstra
+        dijkstra(0, 0, heights);
 
-        return Arrays.stream(d[n - 1][m - 1]).min().orElse(0);
+        return d[n-1][m-1];
     }
 
-    public void dijkstra(int startX, int startY, int n, int m, int[][] board) {
-        PriorityQueue<Info> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.cost));
-
-        for(int i=0; i<4; i++) {
-            d[startX][startY][i] = 0;
-            pq.add(new Info(startX, startY, 0, i));
-        }
+    public void dijkstra(int startX, int startY, int[][] heights) {
+        PriorityQueue<Point> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.cost));
+        d[startX][startY] = 0;
+        pq.add(new Point(startX, startY, 0));
 
         while(!pq.isEmpty()) {
-            Info cur = pq.poll();
+            Point cur = pq.poll();
             int x = cur.x;
             int y = cur.y;
-            int dir = cur.dir;
+            int cost = cur.cost;
 
-            for(int i=0; i<4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+            if(d[x][y] < cost) continue;
 
-                if(nx < 0 || nx >= n || ny < 0 || ny >=m) continue;
-                if(board[nx][ny] == 1) continue;
+            for(int[] da: dir) {
+                int nx = x + da[0];
+                int ny = y + da[1];
 
-                int nCost = dir == i ? 100 : 600;
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
 
-                if(d[nx][ny][i] > d[x][y][dir] + nCost) {
-                    d[nx][ny][i] = d[x][y][dir] + nCost;
-                    pq.add(new Info(nx, ny, nCost, i));
+                int nCost = Math.max(Math.abs(heights[nx][ny] - heights[x][y]), d[x][y]);
+
+                if(d[nx][ny] > nCost) {
+                    d[nx][ny] = nCost;
+                    pq.add(new Point(nx, ny, d[nx][ny]));
                 }
             }
         }
     }
 }
 
-class Info {
-    int x, y, cost, dir;
-    public Info(int x, int y, int cost, int dir) {
+class Point {
+    int x, y, cost;
+    public Point(int x, int y, int cost) {
         this.x = x;
         this.y = y;
         this.cost = cost;
-        this.dir = dir;
     }
 }
