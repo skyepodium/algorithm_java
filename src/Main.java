2777379222205dbae1710cab2a2bc4a44b045461
skyepodium <skyepodium@gmail.com>
2022-04-28 14:13:22 +0900

    프로그래머스 - 경주로 건설

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,77 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
+import java.util.PriorityQueue;
 
 class Solution {
-    int[] d;
-    public int minCostConnectPoints(int[][] points) {
+    int[][][] d;
+    int[] dx = {0, 0, 1, -1};
+    int[] dy = {-1, 1, 0, 0};
+
+    public int solution(int[][] board) {
         // 1. init
-        int n = points.length;
-        d = new int[n];
-        for(int i=0; i<n; i++) d[i] = i;
-        List<Point> l = new ArrayList<>();
-        int res = 0;
+        int n = board.length;
+        int m = board[0].length;
+        int maxVal = n * m * 600;
+        d = new int[n][m][4];
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++)
+                for(int k=0; k<4; k++)
+                    d[i][j][k] = maxVal;
 
-        // 2. make graph
-        for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                int dist = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
-                l.add(new Point(i, j, dist));
-            }
-        }
+        dijkstra(0, 0, n, m, board);
 
-        // 3. sort
-        l.sort(Comparator.comparingInt(a -> a.cost));
-
-        // 4. cost
-        for(Point p: l) {
-            int s = find(p.start);
-            int e = find(p.end);
-            int c = p.cost;
-
-            if(s != e) {
-                d[s] = e;
-                res += c;
-            }
-        }
-
-        return res;
+        return Arrays.stream(d[n - 1][m - 1]).min().orElse(0);
     }
 
-    public int find(int node) {
-        if(node == d[node]) return node;
-        else return d[node] = find(d[node]);
+    public void dijkstra(int startX, int startY, int n, int m, int[][] board) {
+        PriorityQueue<Info> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.cost));
+
+        for(int i=0; i<4; i++) {
+            d[startX][startY][i] = 0;
+            pq.add(new Info(startX, startY, 0, i));
+        }
+
+        while(!pq.isEmpty()) {
+            Info cur = pq.poll();
+            int x = cur.x;
+            int y = cur.y;
+            int dir = cur.dir;
+
+            for(int i=0; i<4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if(nx < 0 || nx >= n || ny < 0 || ny >=m) continue;
+                if(board[nx][ny] == 1) continue;
+
+                int nCost = dir == i ? 100 : 600;
+
+                if(d[nx][ny][i] > d[x][y][dir] + nCost) {
+                    d[nx][ny][i] = d[x][y][dir] + nCost;
+                    pq.add(new Info(nx, ny, nCost, i));
+                }
+            }
+        }
     }
 }
 
-class Point {
-    int start;
-    int end;
-    int cost;
-    public Point(int start, int end, int cost) {
-        this.start = start;
-        this.end = end;
+class Info {
+    int x, y, cost, dir;
+    public Info(int x, int y, int cost, int dir) {
+        this.x = x;
+        this.y = y;
         this.cost = cost;
+        this.dir = dir;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+
+        Solution sl = new Solution();
+        int[][] board = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        int res = sl.solution(board);
+
+        System.out.println("res " + res);
     }
 }
