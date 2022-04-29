c62abaa69f8bf0dd8eb482e38510e975e5608a8d
skyepodium <skyepodium@gmail.com>
2022-04-29 13:03:31 +0900

    leetcode - special-array-with-x-elements-greater-than-or-equal-x

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,35 @@
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 
 class Solution {
-    int n, m;
-    int[][] d;
-    int[][] dir = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
-    public int minimumEffortPath(int[][] heights) {
+    public int specialArray(int[] nums) {
         // 1. init
-        int maxVal = 1000000;
-        n = heights.length;
-        m = heights[0].length;
-        d = new int[n][m];
-        for(int i=0; i<n; i++)
-            for(int j=0; j<m; j++)
-                d[i][j] = maxVal;
+        int maxVal = Arrays.stream(nums).max().orElse(0);
+        int res = -1;
 
-        // 2. dijkstra
-        dijkstra(0, 0, heights);
-
-        return d[n-1][m-1];
-    }
-
-    public void dijkstra(int startX, int startY, int[][] heights) {
-        PriorityQueue<Point> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.cost));
-        d[startX][startY] = 0;
-        pq.add(new Point(startX, startY, 0));
-
-        while(!pq.isEmpty()) {
-            Point cur = pq.poll();
-            int x = cur.x;
-            int y = cur.y;
-            int cost = cur.cost;
-
-            if(d[x][y] < cost) continue;
-
-            for(int[] da: dir) {
-                int nx = x + da[0];
-                int ny = y + da[1];
-
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-
-                int nCost = Math.max(Math.abs(heights[nx][ny] - heights[x][y]), d[x][y]);
-
-                if(d[nx][ny] > nCost) {
-                    d[nx][ny] = nCost;
-                    pq.add(new Point(nx, ny, d[nx][ny]));
-                }
+        // 2. binary search
+        int s = 0, e = maxVal;
+        while(s <= e) {
+            int mid = s + (e - s) / 2;
+            int cnt = calCnt(mid, nums);
+            if(cnt > mid) {
+                s = mid + 1;
+            }
+            else if(cnt == mid) {
+                return mid;
+            }
+            else {
+                e = mid - 1;
             }
         }
-    }
-}
 
-class Point {
-    int x, y, cost;
-    public Point(int x, int y, int cost) {
-        this.x = x;
-        this.y = y;
-        this.cost = cost;
+        return res;
+    }
+
+    public int calCnt(int mid, int[] nums) {
+        int res = 0;
+        for(int x: nums) {
+            if(x >= mid) res++;
+        }
+        return res;
     }
 }
