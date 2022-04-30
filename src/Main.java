ab5d6a12ae747b46d192622ebbd30a6d17545637
skyepodium <skyepodium@gmail.com>
2022-05-01 00:25:29 +0900

    프로그래머스 - 도둑질

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,22 @@
-import java.util.*;
-
 class Solution {
-    List<List<Integer>> res = new ArrayList<>();
-    boolean[] check;
-    int n;
-    Set<String> s = new HashSet<>();
-    public List<List<Integer>> permuteUnique(int[] nums) {
-        n = nums.length;
-        check = new boolean[n];
+    public int solution(int[] money) {
+        // 1. init
+        int n = money.length;
+        int[] a = new int[n];
+        int[] b = new int[n];
+        a[0] = money[0];
+        a[1] = money[0];
+        b[1] = money[1];
 
-        dfs(new Stack<>(), 0, nums);
-
-        return res;
-    }
-
-    public void dfs(Stack<Integer> l, int cnt, int[] nums) {
-        if(cnt >= n) {
-            List<Integer> cur = new ArrayList<>(l);
-            StringBuilder key = new StringBuilder();
-            for(int i=0; i<cur.size(); i++) {
-                key.append(cur.get(i).toString());
-            }
-            if(!s.contains(key.toString())) {
-                s.add(key.toString());
-                res.add(cur);
-            }
-            return;
+        // 2. bottom up
+        for(int i=2; i<n-1; i++) {
+            a[i] = Math.max(a[i-2] + money[i], a[i-1]);
         }
 
-        for(int i=0; i<n; i++) {
-            if(!check[i]) {
-                check[i] = true;
-                l.push(nums[i]);
-                dfs(l, cnt + 1, nums);
-                l.pop();
-                check[i] = false;
-            }
+        for(int i=2; i<n; i++) {
+            b[i] = Math.max(b[i-2] + money[i], b[i-1]);
         }
-    }
-}
 
-class Main {
-    public static void main(String[] args) {
-        Solution sl = new Solution();
-        int[] nums = {1,1,2};
-
-        sl.permuteUnique(nums);
+        return Math.max(a[n-2], b[n-1]);
     }
 }
