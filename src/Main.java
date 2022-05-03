46c93e285377f6a70a8883601968ae4108ccc873
skyepodium <skyepodium@gmail.com>
2022-05-03 11:56:24 +0900

    leetcode - palindrome-number

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,7 @@
-import java.util.Stack;
-
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
-        return refine(s).equals(refine(t));
-    }
+    public boolean isPalindrome(int x) {
+        String s = Integer.toString(x);
 
-    public String refine(String s) {
-        Stack<Character> st = new Stack<>();
-        for(int i=0; i<s.length(); i++) {
-            char cur = s.charAt(i);
-            if(cur != '#') {
-                st.add(cur);
-            }
-            else {
-                if(!st.isEmpty()) st.pop();
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-        while(!st.isEmpty()) {
-            sb.append(st.pop());
-        }
-        return sb.toString();
+        return s.equals(new StringBuffer(s).reverse().toString());
     }
 }
