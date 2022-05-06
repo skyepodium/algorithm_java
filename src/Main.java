ffac53b7b1e2a1ca315bf13d5e517a8813f4d248
skyepodium <skyepodium@gmail.com>
2022-05-06 22:17:55 +0900

    leetcode - remove-all-adjacent-duplicates-in-string

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,24 @@
-class Solution {
-    public boolean checkStraightLine(int[][] coordinates) {
-        // 1. init
-        int n = coordinates.length;
+import java.util.Stack;
 
-        // 2. loop
-        for(int i=0; i<n-2; i++) {
-            if(ccw(new Point(coordinates[i][0], coordinates[i][1]), new Point(coordinates[i+1][0], coordinates[i+1][1]), new Point(coordinates[i+2][0], coordinates[i+2][1])) != 0) return false;
+class Solution {
+    public String removeDuplicates(String s) {
+        // 1. init
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+
+            if(st.size() > 0 && st.peek() == c) {
+                while(st.size() > 0 && st.peek() == c) st.pop();
+            }
+            else {
+                st.add(c);
+            }
         }
 
-        return true;
-    }
-
-    public int ccw(Point p, Point q, Point r) {
-        long first = (p.x - r.x) * (q.y - r.y);
-        long second = (p.y - r.y) * (q.x - r.x);
-        long result = first - second;
-
-        if(result > 0) return 1;
-        else if(result == 0) return 0;
-        else return -1;
-    }
-}
-
-class Point {
-    long x, y;
-    public Point(long x, long y) {
-        this.x = x;
-        this.y = y;
+        // 3. res
+        StringBuilder res = new StringBuilder();
+        while(st.size() > 0) res.append(st.pop());
+        return res.reverse().toString();
     }
 }
