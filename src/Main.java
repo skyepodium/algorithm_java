fe9a59c91b7dd5dbe5b3daafcfb1613f8fcb2b87
skyepodium <skyepodium@gmail.com>
2022-05-07 14:18:58 +0900

    wave

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,13 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    public int maxOperations(int[] nums, int k) {
+    public int largestAltitude(int[] gain) {
         // 1. init
-        Map<Integer, Integer> m = new HashMap<>();
-        Arrays.stream(nums).forEach(x -> m.put(x, m.getOrDefault(x, 0) + 1));
-        int n = nums.length;
+        int prev = 0;
         int res = 0;
 
         // 2. loop
-        for(int num: nums) {
-            int remain = k - num;
-            if(num == remain && m.getOrDefault(remain, 0) <= 1) continue;
-            if(m.getOrDefault(remain, 0) < 1 || m.getOrDefault(num, 0) < 1) continue;
-
-            res++;
-            m.put(remain, m.get(remain) - 1);
-            m.put(num, m.get(num) - 1);
+        for(int g: gain) {
+            prev = prev + g;
+            res = Math.max(res, prev);
         }
 
         return res;
