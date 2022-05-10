4ce98e27289b830638bfbf21205b42bfa6520449
skyepodium <skyepodium@gmail.com>
2022-05-10 18:16:36 +0900

    leetcode -  combination-sum-iii

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-class Solution {
-    public int largestAltitude(int[] gain) {
-        // 1. init
-        int prev = 0;
-        int res = 0;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
-        // 2. loop
-        for(int g: gain) {
-            prev = prev + g;
-            res = Math.max(res, prev);
-        }
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    boolean[] check = new boolean[10];
+    public List<List<Integer>> combinationSum3(int k, int n) {
+
+        go(0, new Stack<>(), 0, n, k);
 
         return res;
+    }
+
+    public void go(int cnt, Stack<Integer> s, int sumVal, int n, int k) {
+        if(sumVal > n) return;
+
+        if(cnt >= k) {
+            if(sumVal == n)
+                res.add(new ArrayList<>(s));
+            return;
+        }
+
+        for(int i=1; i<10; i++) {
+            if(!check[i]) {
+                if(!s.isEmpty() && s.peek() > i) continue;
+
+                check[i] = true;
+                s.add(i);
+                go(cnt + 1, s, sumVal + i, n, k);
+                check[i] = false;
+                s.pop();
+            }
+        }
     }
 }
