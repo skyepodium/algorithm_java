3491801cdd17631358176f1cdfee942dbb5bd2da
skyepodium <skyepodium@gmail.com>
2022-05-29 17:26:29 +0900

    leetcode - maximum-product-of-word-lengths

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,27 @@
 class Solution {
-    public int countSubstrings(String s) {
+    public int maxProduct(String[] words) {
         // 1. init
-        int n = s.length();
+        int n = words.length;
+        int[] w = new int[n];
+
+        for(int i=0; i<n; i++) {
+            String wo = words[i];
+            int bit = 0;
+            for(String c: wo.split("")) {
+                int val = (int)c.charAt(0) - (int)"a".charAt(0);
+                bit |= 1 << val;
+            }
+            w[i] = bit;
+        }
         int res = 0;
 
         // 2. loop
         for(int i=0; i<n; i++) {
-            res += isPalindrome(i, i, n, s);
-            res += isPalindrome(i, i+1, n, s);
-        }
+            for(int j=i+1; j<n; j++) {
+                if((w[i] & w[j]) != 0) continue;
 
-        return res;
-    }
-
-    public int isPalindrome(int l, int r, int n, String s) {
-        int res = 0;
-
-        while(l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
-            res++;
-            l--;
-            r++;
+                res = Math.max(res, words[i].length() * words[j].length());
+            }
         }
 
         return res;
