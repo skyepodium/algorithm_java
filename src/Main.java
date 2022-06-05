86dcb221b49e19fe992cbb90a74706fce9af6f90
skyepodium <skyepodium@gmail.com>
2022-06-05 23:22:14 +0900

    leetcode - longest-nice-substring

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,36 @@ import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
-    public boolean hasAllCodes(String s, int k) {
+    public String longestNiceSubstring(String s) {
         // 1. init
         int n = s.length();
-        int cnt = 1 << k;
-        Set<String> seen = new HashSet<>();
+        String res = "";
 
         // 2. loop
-        for(int i=0; i<n-k+1; i++) {
-            String cur = s.substring(i, i+k);
-            if(!seen.contains(cur)) {
-                cnt--;
-                seen.add(cur);
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n+1; j++) {
+                // 1) set
+                boolean isNice = true;
+                String cur = s.substring(i, j);
+                Set<Character> se = new HashSet<>();
+                for(int k=0; k<cur.length(); k++) {
+                    char c = cur.charAt(k);
+                    se.add(c);
+                }
+
+                for(int k=0; k<cur.length(); k++) {
+                    char c = cur.charAt(k);
+                    if(((int)c >= (int)'a' && !se.contains((char)(c - 32)))
+                    || ((int)c < (int)'a' && !se.contains((char)(c + 32)))) {
+                        isNice = false;
+                        break;
+                    }
+                }
+
+                if(isNice && res.length() < cur.length()) res = cur;
             }
-            if(cnt == 0) return true;
         }
-        
-        return false;
+
+        return res;
     }
 }
