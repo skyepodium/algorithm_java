7af2b1d46a5b077782d67029d63894f21d9c3ee8
skyepodium <skyepodium@gmail.com>
2022-11-22 16:58:05 +0900

    leetcode - binary-tree-maximum-path-sum

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,35 @@
-import java.util.Arrays;
-import java.util.HashSet;
-
-class Solution {
-    public int openLock(String[] deadends, String target) {
-
-        return bfs("0000", target, deadends);
-    }
-
-    public String getNextNode(String node, int index, int direction) {
-        StringBuilder sb = new StringBuilder(node);
-
-        // up
-        int nextBit = 0;
-        if(direction == 0) {
-            nextBit = (node.charAt(index) - '0' + 1) % 10;
-        }
-
-        // down
-        if(direction == 1) {
-            nextBit = (node.charAt(index) - '0' - 1 + 10) % 10;
-        }
-
-        return sb.replace(index, index + 1, String.valueOf(nextBit)).toString();
-    }
-
-    public int bfs(String startNode, String endNode, String[] deadends) {
-        HashSet<String> q1 = new HashSet<>();
-        HashSet<String> q2 = new HashSet<>();
-        q1.add(startNode);
-        q2.add(endNode);
-
-        HashSet<String> visited = new HashSet<>(Arrays.asList(deadends));
-        int step = 0;
-
-        while(!q1.isEmpty() && !q2.isEmpty()) {
-            if (q1.size() > q2.size()) {
-                HashSet<String> tmp = q1;
-                q1 = q2;
-                q2 = tmp;
-            }
-
-            HashSet<String> temp = new HashSet<>();
-
-            for(String node: q1) {
-                if(visited.contains(node)) continue;
-                if(q2.contains(node)) return step;
-                visited.add(node);
-
-                for(int i=0; i<4; i++) {
-                    for(int j=0; j<2; j++) {
-                        String nextNode = getNextNode(node, i, j);
-                        if(visited.contains(nextNode)) continue;
-                        System.out.println("nextNode " + nextNode);
-                        temp.add(nextNode);
-                    }
-                }
-            }
-            q1 = q2;
-            q2 = temp;
-            step++;
-        }
-        return -1;
-    }
+public class TreeNode {
+   int val;
+    TreeNode left;
+   TreeNode right;
+  TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+  TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+        this.left = left;
+        this.right = right;
+     }
 }
 
-class Main {
-    public static void main(String[] args) {
-        String[] deadends = {"0201","0101","0102","1212","2002"};
-        String target = "0202";
+class Solution {
+    private int res;
 
-        Solution sl = new Solution();
-        System.out.println(sl.openLock(deadends, target));
+    public Solution() {
+        this.res = Integer.MIN_VALUE;
+    }
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return this.res;
+    }
+
+    public int dfs(TreeNode node) {
+        if(node == null) return 0;
+
+        int l = Math.max(0, dfs(node.left));
+        int r = Math.max(0, dfs(node.right));
+
+        this.res = Math.max(this.res, l + r + node.val);
+
+        return Math.max(l, r) + node.val;
     }
 }
